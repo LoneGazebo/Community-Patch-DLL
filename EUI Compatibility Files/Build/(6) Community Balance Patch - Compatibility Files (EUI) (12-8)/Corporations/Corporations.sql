3bd41659f5371205d27eba9f5fe85d9b4d7c82ff
GrantSP <fr00gyl@gmail.com>
2016-02-09 17:54:57 +1100

    TwoKay Foods spelling mistake of coffee
    Coffee was spelled "coffe" in the pedia. I've changed all the
    occurrences of it but as most are archived versions, it isn't as large
    as the 22 times appears.

--- EUI Compatibility Files/Build/(6) Community Balance Patch - Compatibility Files (EUI) (12-8)/Corporations/Corporations.sql
+++ EUI Compatibility Files/Build/(6) Community Balance Patch - Compatibility Files (EUI) (12-8)/Corporations/Corporations.sql
@@ -93,5 +93,5 @@ SET Text = 'Requires a Global Monopoly on [ICON_RES_WINE] Wine, [ICON_RES_TRUFFL
 WHERE Tag = 'TXT_KEY_BUILDING_TWOKAY_FOODS_HQ_HELP';
 
 UPDATE Language_en_US
-SET Text = 'Resource Bonus: [ICON_RES_WINE] Wine, [ICON_RES_TRUFFLES] Truffles, [ICON_RES_CITRUS] Citrus, [ICON_RES_OLIVE] Olives, [ICON_RES_COFFEE] Coffe, and [ICON_RES_TEA] Tea gain additional [ICON_FOOD] Food.[NEWLINE][NEWLINE]Office Bonus: +3 [ICON_FOOD] Food for every Global Franchise (up to your Franchise Limit).[NEWLINE][NEWLINE]Trade Route Bonus: Trade Routes to a foreign City with a TwoKay Foods Franchise increases the origin City''s [ICON_FOOD] Growth rate by +10%, and owned Trade Routes have +1 Sight and reveal the Fog of War.'
+SET Text = 'Resource Bonus: [ICON_RES_WINE] Wine, [ICON_RES_TRUFFLES] Truffles, [ICON_RES_CITRUS] Citrus, [ICON_RES_OLIVE] Olives, [ICON_RES_COFFEE] Coffee, and [ICON_RES_TEA] Tea gain additional [ICON_FOOD] Food.[NEWLINE][NEWLINE]Office Bonus: +3 [ICON_FOOD] Food for every Global Franchise (up to your Franchise Limit).[NEWLINE][NEWLINE]Trade Route Bonus: Trade Routes to a foreign City with a TwoKay Foods Franchise increases the origin City''s [ICON_FOOD] Growth rate by +10%, and owned Trade Routes have +1 Sight and reveal the Fog of War.'
 WHERE Tag = 'TXT_KEY_CORP_HELPER_TWOKAY_FOODS';
