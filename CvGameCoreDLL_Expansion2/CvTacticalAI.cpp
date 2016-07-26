d1a0d7d39aa5304b87f6b1b7edea570de4e55855
ilteroi <ilteroi@web.de>
2016-07-26 08:08:24 +0200

    fix a typo

--- CvGameCoreDLL_Expansion2/CvTacticalAI.cpp
+++ CvGameCoreDLL_Expansion2/CvTacticalAI.cpp
@@ -10806,7 +10806,7 @@ int CvTacticalAI::ScoreHedgehogPlots(CvPlot* pTarget)
 
 				bool bMyCity = (pPlot->isCity() && pPlot->getOwner()==m_pPlayer->GetID());
 				if (bMyCity)
-					iScore -= 1000;
+					iScore += 1000;
 
 				//we want to be able to attack a lot of plots
 				if (pPlot->GetNumEnemyUnitsAdjacent(m_pPlayer->getTeam(), pPlot->getDomain())>0 && !bMyCity)
