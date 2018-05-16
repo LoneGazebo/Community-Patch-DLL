b7e5a97b4095e7125cbdd4c2f9f45e03031e1de1
HungryForFood <jj-foo@hotmail.com>
2018-05-16 14:08:31 +0800

    Victoria changes 16/5/2018 - Fixed UI bug for science gain from trading with influence civilization (missing divide by 100)

--- Community Balance Patch/Modular Elements/TradeUIMod/TradeRouteHelpers.lua
+++ Community Balance Patch/Modular Elements/TradeUIMod/TradeRouteHelpers.lua
@@ -322,7 +322,7 @@ function BuildTradeRouteScienceToolTipString (pOriginCity, pTargetCity, eDomain)
 			strResult = strResult .. Locale.ConvertTextKey("TXT_KEY_CHOOSE_GOLD_INTERNAL_TRADE_ROUTE_ITEM_TT_YOUR_SCIENCE_GAIN", iOriginScience);
 		else	
 			local iNumTechs = pOriginPlayer:GetNumTechDifference(iOtherPlayer);
-			local iInfluenceScience = pOriginPlayer:GetInfluenceTradeRouteScienceBonus(iOtherPlayer);
+			local iInfluenceScience = pOriginPlayer:GetInfluenceTradeRouteScienceBonus(iOtherPlayer) / 100;
 			local delta = iOriginScience - iInfluenceScience;
 			if (iPlayer == Game.GetActivePlayer()) then	
 				strResult = strResult .. Locale.ConvertTextKey("TXT_KEY_CHOOSE_INTERNATIONAL_TRADE_ROUTE_ITEM_TT_YOUR_SCIENCE_GAIN");
@@ -362,7 +362,7 @@ function BuildTradeRouteScienceToolTipString (pOriginCity, pTargetCity, eDomain)
 		end
 	
 		local iNumTechs = pOtherPlayer:GetNumTechDifference(iPlayer);
-		local iInfluenceScience = pOtherPlayer:GetInfluenceTradeRouteScienceBonus(iPlayer);
+		local iInfluenceScience = pOtherPlayer:GetInfluenceTradeRouteScienceBonus(iPlayer) / 100;
 		local delta = iDestScience - iInfluenceScience;
 		if (iPlayer == Game.GetActivePlayer()) then
 			strResult = strResult .. Locale.ConvertTextKey("TXT_KEY_CHOOSE_INTERNATIONAL_TRADE_ROUTE_ITEM_TT_THEIR_SCIENCE_GAIN");
