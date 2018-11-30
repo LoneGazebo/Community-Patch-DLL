2e08fba2de837d0e5fd3704246d50d60fcda5547
TechpriestEnginseer <techpriest8@gmail.com>
2018-11-30 07:44:41 -0800

    Update CivText.sql
    https://github.com/LoneGazebo/Community-Patch-DLL/issues/4939

--- Community Balance Patch/Balance Changes/Text/en_US/CivText.sql
+++ Community Balance Patch/Balance Changes/Text/en_US/CivText.sql
@@ -894,6 +894,9 @@ UPDATE Language_en_US
 SET Text = 'May Sell Exotic Goods when adjacent to City-State lands to gain [ICON_GOLD] Gold and XP based on distance from capital. Each Nau can perform this action twice. [NEWLINE][NEWLINE]If possible, when a Nau sells its Exotic Cargo, a Feitoria is also [COLOR_POSITIVE_TEXT]automatically created[ENDCOLOR] in its territory.'
 WHERE Tag = 'TXT_KEY_PROMOTION_SELL_EXOTIC_GOODS_HELP' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_LEADERS' AND Value= 1 );
 
+UPDATE Language_en_US 
+SET Text = 'This unit will sell its cargo, earning [ICON_GOLD] Gold and XP. More will be earned for selling farther from your capital. This may only be done twice with this unit.'
+WHERE Tag = 'TXT_KEY_MISSION_SELL_EXOTIC_GOODS_HELP' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_LEADERS' AND Value= 1 );
 --------------------
 -- Rome
 --------------------
