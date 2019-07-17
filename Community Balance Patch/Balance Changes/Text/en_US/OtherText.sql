6543b3c0ba2597f87878dc7195562e8fc4881215
Aqinu <49090452+Aqinu@users.noreply.github.com>
2019-07-17 12:25:49 +0300

    Fix for issue #5442
    I've compacted the information from all the World Wonders so that they can fit better and not overflow in the Popup Window when the Wonder is completed..
    
    Some Wonders still overflow but now are just a few (Red Fort, Parthenon, Globe Theatre, CN Tower, Bletchley Park, it's just too much text)
    
    Also I added missing icons from the descriptions (outside of Military Unit Supply and Urbanization, cause I noticed that icons from these still change)
    
    And also I made a general format for the descriptions like this:
    [REQUIREMENT][FREE STUFF][ADDITIONAL BONUSES]
    
    Also changed Apollo and Manhattan Projects to fit to the format
    
    Fixed bullet alignment and color issue of Citizen Earth Protocol Project

--- Community Balance Patch/Balance Changes/Text/en_US/OtherText.sql
+++ Community Balance Patch/Balance Changes/Text/en_US/OtherText.sql
@@ -133,7 +133,7 @@ SET Text = 'You have moved beyond the Modern Era. The ideas of modernity now per
 WHERE Tag = 'TXT_KEY_NOTIFICATION_CHOOSE_IDEOLOGY_ERA' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_TOURISM_CHANGES' AND Value= 1 );
 	
 UPDATE Language_en_US
-SET Text = 'Allows your Cities to build Atomic Bombs and Nuclear Missiles.[NEWLINE][NEWLINE]If you are the [COLOR_POSITIVE_TEXT]first[ENDCOLOR] to complete this, receive a free Research Lab and Atomic Bomb in your [ICON_CAPITAL] Capital.'
+SET Text = 'Allows your Cities to build [COLOR_POSITIVE_TEXT]Atomic Bombs[ENDCOLOR] and [COLOR_POSITIVE_TEXT]Nuclear Missiles[ENDCOLOR].[NEWLINE][NEWLINE]If you are the [COLOR_POSITIVE_TEXT]First[ENDCOLOR] to complete this, receive 1 [COLOR_POSITIVE_TEXT]Free[ENDCOLOR] Atomic Bomb and a [COLOR_POSITIVE_TEXT]Free[ENDCOLOR] Research Lab in your [ICON_CAPITAL] Capital.'
 WHERE Tag = 'TXT_KEY_PROJECT_MANHATTAN_PROJECT_HELP' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_LEADERS' AND Value= 1 );
 
 UPDATE Language_en_US
@@ -141,7 +141,7 @@ SET Text = 'The Manhattan Project allows a civilization to construct nuclear wea
 WHERE Tag = 'TXT_KEY_PROJECT_MANHATTAN_PROJECT_STRATEGY' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_LEADERS' AND Value= 1 );
 
 UPDATE Language_en_US
-SET Text = 'Signals the start of the space race, allowing your Cities to build spaceship parts. When the spaceship is completed, you win a [COLOR_POSITIVE_TEXT]Science Victory[ENDCOLOR]![NEWLINE][NEWLINE]If you are the [COLOR_POSITIVE_TEXT]first[ENDCOLOR] to complete this, receive a free [ICON_GREAT_SCIENTIST] Great Scientist near your [ICON_CAPITAL] Capital, and a [ICON_GOLDEN_AGE] Golden Age begins.'
+SET Text = 'Signals the start of the space race, allowing your Cities to build Spaceship parts. When the Spaceship is completed, you win a [COLOR_POSITIVE_TEXT]Science Victory[ENDCOLOR]![NEWLINE][NEWLINE]If you are the [COLOR_POSITIVE_TEXT]First[ENDCOLOR] to complete this, receive 1 [COLOR_POSITIVE_TEXT]Free[ENDCOLOR] [ICON_GREAT_SCIENTIST] Great Scientist near your [ICON_CAPITAL] Capital, and your Empire enters a [ICON_GOLDEN_AGE] Golden Age.'
 WHERE Tag = 'TXT_KEY_PROJECT_APOLLO_PROGRAM_HELP' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_LEADERS' AND Value= 1 );
 
 UPDATE Language_en_US
