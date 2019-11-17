e364a9dcc43038b5716f9384724235231153a879
HeathcliffWarriors <38794241+HeathcliffWarriors@users.noreply.github.com>
2019-11-17 14:09:26 -0500

    Uncomment same master diplo mod

--- CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.cpp
@@ -12678,7 +12678,6 @@ int CvLuaPlayer::lGetOpinionTable(lua_State* L)
 				kOpinion.m_str = Localization::Lookup("TXT_KEY_DIPLO_TOO_MANY_VASSALS");
 				aOpinions.push_back(kOpinion);
 			}
-			/*
 			iValue = pDiploAI->GetSameMasterScore(eWithPlayer);
 			if (iValue != 0)
 			{
@@ -12687,7 +12686,6 @@ int CvLuaPlayer::lGetOpinionTable(lua_State* L)
 				kOpinion.m_str = Localization::Lookup("TXT_KEY_DIPLO_SAME_MASTER");
 				aOpinions.push_back(kOpinion);
 			}
-			*/
 		}
 #endif
 	}
