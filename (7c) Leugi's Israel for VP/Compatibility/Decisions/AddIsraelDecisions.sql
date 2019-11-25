4257d3609b252a1026768e63440fb31812f7e75e
pineappledan <33136027+pineappledan@users.noreply.github.com>
2019-11-25 06:44:09 -0700

    David
    Adds a (7c) folder containing Israel led by King David

---
+++ (7c) Leugi's Israel for VP/Compatibility/Decisions/AddIsraelDecisions.sql
@@ -0,0 +1,5 @@
+--==========================================================================================================================
+-- DecisionsAddin_Support
+--==========================================================================================================================
+CREATE TABLE IF NOT EXISTS DecisionsAddin_Support(FileName);
+INSERT INTO DecisionsAddin_Support (FileName) VALUES ('IsraelDecisionScriptsLeugi.lua');
