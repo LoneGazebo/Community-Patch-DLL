b406e1cdcc26a35c1bb9a31d5963d26ad05af688
Gazebo <thewizardofwas@gmail.com>
2019-12-01 21:13:10 -0600

    12-1

--- (7c) Leugi's Israel for VP/Compatibility/Decisions/AddIsraelDecisions.sql
+++
@@ -1,5 +0,0 @@
---==========================================================================================================================
--- DecisionsAddin_Support
---==========================================================================================================================
-CREATE TABLE IF NOT EXISTS DecisionsAddin_Support(FileName);
-INSERT INTO DecisionsAddin_Support (FileName) VALUES ('IsraelDecisionScriptsLeugi.lua');
