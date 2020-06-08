491444ad0bb2c970467fcbdc5d57ad2d844e64df
HeathcliffWarriors <38794241+HeathcliffWarriors@users.noreply.github.com>
2020-06-07 20:17:21 -0400

    Small cleanup

--- CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaPlayer.cpp
@@ -13125,7 +13125,6 @@ int CvLuaPlayer::lGetOpinionTable(lua_State* L)
 	iValue = pDiploAI->GetEmbassyScore(eWithPlayer);
 	if (iValue != 0)
 	{
-#if defined(MOD_BALANCE_CORE)
 		if(GET_TEAM(GET_PLAYER(eWithPlayer).getTeam()).HasEmbassyAtTeam(pkPlayer->getTeam()) && GET_TEAM(pkPlayer->getTeam()).HasEmbassyAtTeam(GET_PLAYER(eWithPlayer).getTeam()))
 		{
 			Opinion kOpinion;
@@ -13147,7 +13146,6 @@ int CvLuaPlayer::lGetOpinionTable(lua_State* L)
 			kOpinion.m_str = Localization::Lookup("TXT_KEY_DIPLO_WE_HAVE_EMBASSY");
 			aOpinions.push_back(kOpinion);
 		}
-#endif
 	}
 
 	iValue = pDiploAI->GetForgaveForSpyingScore(eWithPlayer);
