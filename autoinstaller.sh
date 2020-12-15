4745ace25137e42267d48be76bf1ef3a2390efc1
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-12-15 13:48:53 +0100

    autoinstaller.sh: todo

--- autoinstaller.sh
+++ autoinstaller.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo "Hello World"
+
+# 1. Move "LUA" folder from "Community Patch" to main folder "CP LUA"
+# 2. Move "LUA" folder from "Community Balance Patch" to main folder "CBO LUA".
+# 3. Modify "CPPSetupData.iss" to reflect correct folder naming structure.
+# 4. Test the installer locally.
+# 5. Push changes to "github actions".
+# 6. What about "UI_bc1" folder?
