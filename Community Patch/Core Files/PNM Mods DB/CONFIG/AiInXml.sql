d1a0403b5ae8e516bfe211887f6c54a8d8b9cc6c
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2021-01-12 05:34:51 -0500

    Remove unused values

--- Community Patch/Core Files/PNM Mods DB/CONFIG/AiInXml.sql
+++ Community Patch/Core Files/PNM Mods DB/CONFIG/AiInXml.sql
@@ -3,9 +3,4 @@ INSERT INTO Defines(Name, Value) VALUES('AI_CONFIG_MILITARY_MELEE_PER_AA', 2);
 -- Number of water tiles per ship for (small?) bodies of water, the default is 5, but 7 or 8 would be sufficient
 INSERT INTO Defines(Name, Value) VALUES('AI_CONFIG_MILITARY_TILES_PER_SHIP', 6);
 
-
--- Priority caller for homeland AI secondary settler
-INSERT INTO Defines(Name, Value) VALUES('AI_HOMELAND_MOVE_PRIORITY_SECONDARY_SETTLER', 1);
-INSERT INTO Defines(Name, Value) VALUES('AI_HOMELAND_MOVE_PRIORITY_SECONDARY_WORKER', 3);
-
 INSERT INTO CustomModDbUpdates(Name, Value) VALUES('CONFIG_AI_IN_XML', 1);
