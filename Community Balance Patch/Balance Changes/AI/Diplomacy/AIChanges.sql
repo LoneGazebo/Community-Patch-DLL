e56cd9d5ee18e2846e2d1c5681a7b8db4f3f9470
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2021-02-10 16:59:33 -0500

    Fix civilian unit appearance change typos

--- Community Balance Patch/Balance Changes/AI/Diplomacy/AIChanges.sql
+++ Community Balance Patch/Balance Changes/AI/Diplomacy/AIChanges.sql
@@ -1,70 +1,24 @@
-DELETE FROM AICityStrategies
-WHERE Type = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
+DELETE FROM AICityStrategies WHERE Type = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
+DELETE FROM AICityStrategies WHERE Type = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
+DELETE FROM AICityStrategy_Flavors WHERE AICityStrategyType = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
+DELETE FROM AICityStrategy_Flavors WHERE AICityStrategyType = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
 
-DELETE FROM AICityStrategies
-WHERE Type = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
+UPDATE AICityStrategy_Flavors SET Flavor = '100' WHERE AICityStrategyType = 'AICITYSTRATEGY_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
+UPDATE AICityStrategy_Flavors SET Flavor = '100' WHERE AICityStrategyType = 'AICITYSTRATEGY_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
 
-DELETE FROM AICityStrategy_Flavors
-WHERE AICityStrategyType = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
-
-DELETE FROM AICityStrategy_Flavors
-WHERE AICityStrategyType = 'AICITYSTRATEGY_NO_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
-
-UPDATE AICityStrategy_Flavors
-SET Flavor = '100' WHERE AICityStrategyType = 'AICITYSTRATEGY_NEED_INTERNATIONAL_LAND_TRADE_ROUTE';
-
-UPDATE AICityStrategy_Flavors
-SET Flavor = '100' WHERE AICityStrategyType = 'AICITYSTRATEGY_NEED_INTERNATIONAL_SEA_TRADE_ROUTE';
-
-UPDATE AICityStrategies
-SET TechPrereq = 'TECH_AGRICULTURE' WHERE Type = 'AICITYSTRATEGY_FIRST_FAITH_BUILDING';
+UPDATE AICityStrategies SET TechPrereq = 'TECH_AGRICULTURE' WHERE Type = 'AICITYSTRATEGY_FIRST_FAITH_BUILDING';
 
 -- Era Diplo Values
+UPDATE Eras SET DiploEmphasisReligion = '2' WHERE Type = 'ERA_ANCIENT';
+UPDATE Eras SET DiploEmphasisReligion = '4' WHERE Type = 'ERA_CLASSICAL';
+UPDATE Eras SET DiploEmphasisReligion = '6' WHERE Type = 'ERA_MEDIEVAL';
+UPDATE Eras SET DiploEmphasisReligion = '5' WHERE Type = 'ERA_RENAISSANCE';
+UPDATE Eras SET DiploEmphasisReligion = '4' WHERE Type = 'ERA_INDUSTRIAL';
+UPDATE Eras SET DiploEmphasisReligion = '3' WHERE Type = 'ERA_MODERN';
+UPDATE Eras SET DiploEmphasisReligion = '2' WHERE Type = 'ERA_POSTMODERN';
+UPDATE Eras SET DiploEmphasisReligion = '1' WHERE Type = 'ERA_FUTURE';
 
-UPDATE Eras
-SET DiploEmphasisReligion = '2' WHERE Type = 'ERA_ANCIENT';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '4' WHERE Type = 'ERA_CLASSICAL';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '6' WHERE Type = 'ERA_MEDIEVAL';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '5' WHERE Type = 'ERA_RENAISSANCE';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '4' WHERE Type = 'ERA_INDUSTRIAL';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '3' WHERE Type = 'ERA_MODERN';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '2' WHERE Type = 'ERA_POSTMODERN';
-
-UPDATE Eras
-SET DiploEmphasisReligion = '1' WHERE Type = 'ERA_FUTURE';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '7' WHERE Type = 'ERA_ANCIENT';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '7' WHERE Type = 'ERA_CLASSICAL';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '7' WHERE Type = 'ERA_MEDIEVAL';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '7' WHERE Type = 'ERA_RENAISSANCE';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '7' WHERE Type = 'ERA_INDUSTRIAL';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '9' WHERE Type = 'ERA_MODERN';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '10' WHERE Type = 'ERA_POSTMODERN';
-
-UPDATE Eras
-SET DiploEmphasisLatePolicies = '8' WHERE Type = 'ERA_FUTURE';
+UPDATE Eras SET DiploEmphasisLatePolicies = '7';
+UPDATE Eras SET DiploEmphasisLatePolicies = '9' WHERE Type = 'ERA_MODERN';
+UPDATE Eras SET DiploEmphasisLatePolicies = '10' WHERE Type = 'ERA_POSTMODERN';
+UPDATE Eras SET DiploEmphasisLatePolicies = '8' WHERE Type = 'ERA_FUTURE';
