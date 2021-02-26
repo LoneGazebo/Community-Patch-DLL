4aa33d42fa22a683873f891742ed629e26ab2575
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2021-02-26 14:02:57 -0500

    Fix a typo

--- Community Balance Patch/Balance Changes/AI/Diplomacy/LeaderCompatibility.sql
+++ Community Balance Patch/Balance Changes/AI/Diplomacy/LeaderCompatibility.sql
@@ -8,7 +8,7 @@ UPDATE Leaders SET Personality = 'PERSONALITY_CONQUEROR' WHERE Type = NEW.Type; 
 UPDATE Leaders SET Personality=CASE
 WHEN (DoFWillingness >= 7 OR Loyalty >= 7) THEN 'PERSONALITY_COALITION'
 WHEN (DiploBalance >= 7 OR MinorCivCompetitiveness >= 7) THEN 'PERSONALITY_DIPLOMAT'
-WHEN (Loyalty >= 3 OR WonderCompetitiveness <= 3) THEN 'PERSONALITY_EXPANSIONIST'
+WHEN (Loyalty <= 3 OR WonderCompetitiveness <= 3) THEN 'PERSONALITY_EXPANSIONIST'
 ELSE Personality END
 WHERE Type = NEW.Type;
 
