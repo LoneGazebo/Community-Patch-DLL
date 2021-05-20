f2aeb95bac0746e5c7a8f9336a67db5252037731
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2021-05-20 12:48:04 -0400

    Final adjustment to peace logic

--- CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
+++ CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
@@ -11402,21 +11402,44 @@ void CvDiplomacyAI::DoUpdatePeaceTreatyWillingness(bool bMyTurn)
 
 		// If we're going for world conquest, we want to fight our wars until we get their capital or can vassalize them
 		// However, do not factor this in when losing
-		if (iWarScore > -15 && bWorldConquest)
+		if (iWarScore > 0 || (iWarScore > -15 && GetPlayerTargetValue(*it) >= TARGET_VALUE_AVERAGE && GetWarState(*it) <= WAR_STATE_DEFENSIVE))
 		{
-			if (bReadyForVassalage || GET_PLAYER(GET_PLAYER(*it).GetCapitalConqueror()).getTeam() == GetTeam())
+			if (bCapturedAnyCityFromUs)
 			{
 				if (iPeaceScore > 0)
-					iPeaceScore *= 2;
-				else
 					iPeaceScore /= 2;
+				else
+					iPeaceScore *= 2;
 			}
-			else
+			else if (bWorldConquest)
+			{
+				if (bReadyForVassalage || GET_PLAYER(GET_PLAYER(*it).GetCapitalConqueror()).getTeam() == GetTeam())
+				{
+					if (iPeaceScore > 0)
+						iPeaceScore *= 2;
+					else
+						iPeaceScore /= 2;
+				}
+				else
+				{
+					if (iPeaceScore > 0)
+						iPeaceScore /= 2;
+					else
+						iPeaceScore *= 2;
+				}
+			}
+			else if (bCapturedAnyCityWeWantToLiberate)
 			{
 				if (iPeaceScore > 0)
-					iPeaceScore /= 2;
+				{
+					iPeaceScore *= 75;
+					iPeaceScore /= 100;
+				}
 				else
-					iPeaceScore *= 2;
+				{
+					iPeaceScore *= 125;
+					iPeaceScore /= 100;
+				}
 			}
 		}
 
