6b22ecf84bacc64677473a450f6bfe5a98d993d6
Milae <milestrevethan@googlemail.com>
2021-05-26 23:21:16 +0100

    Added extra explaination at the end
    - Explains what is being shown in the UI.
    
    Should be the last change to this.

--- CvGameCoreDLL_Expansion2/CvVotingClasses.cpp
+++ CvGameCoreDLL_Expansion2/CvVotingClasses.cpp
@@ -6224,6 +6224,9 @@ CvString CvLeague::GetResolutionProposeOpinionDetails(ResolutionTypes eResolutio
 			s += strOut;
 		}
 	}
+	CvString sEnd = Localization::Lookup("TXT_KEY_LEAGUE_OVERVIEW_PROPOSAL_OPINIONS_END").toUTF8();
+	sEnd.insert(0, "[COLOR_WHITE]");
+	s += sEnd;
 
 	return s;
 }
@@ -6296,10 +6299,11 @@ CvString CvLeague::GetResolutionProposeOpinionDetails(int iTargetResolutionID, P
 			{
 				if (!bNeutralDone)
 				{
-					s += "[NEWLINE]";
+					CvString sNeg = Localization::Lookup("TXT_KEY_LEAGUE_OVERVIEW_PROPOSAL_OPINIONS_NEUTRAL").toUTF8();
+					sNeg.insert(0, "[ENDCOLOR]");
+					s += sNeg;
 					bNeutralDone = true;
 				}
-				strOut.insert(0, "[COLOR_WHITE]");
 				s += strOut;
 			}
 		}
@@ -6328,7 +6332,9 @@ CvString CvLeague::GetResolutionProposeOpinionDetails(int iTargetResolutionID, P
 			s += strOut;
 		}
 	}
-
+	CvString sEnd = Localization::Lookup("TXT_KEY_LEAGUE_OVERVIEW_PROPOSAL_OPINIONS_END").toUTF8();
+	sEnd.insert(0, "[COLOR_WHITE]");
+	s += sEnd;
 
 	return s;
 }
