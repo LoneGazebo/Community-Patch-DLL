8438c3ae9eb78d7cdac4e9bd8529256d3c9d749a
nobubblegums <94949778+nobubblegums@users.noreply.github.com>
2022-02-01 16:43:14 +0300

    Major SQL Cleanup

--- Community Balance Overhaul/Balance Changes/Buildings/BuildingHurryCosts.sql
+++ Community Balance Overhaul/Balance Changes/Buildings/BuildingHurryCosts.sql
@@ -1,9 +1,8 @@
 
 -- Hurry Cost - reduced for all buildings (as you now need production as well to make them work)
 UPDATE Buildings
-SET HurryCostModifier = '5'
-WHERE EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_BUILDINGS' AND Value= 1 );
+SET HurryCostModifier = '5';
 
 UPDATE Buildings
 SET HurryCostModifier = '-5'
-WHERE NOT WonderSplashImage = 'NULL' AND EXISTS (SELECT * FROM COMMUNITY WHERE Type='COMMUNITY_CORE_BALANCE_BUILDINGS' AND Value= 1 );
+WHERE NOT WonderSplashImage = 'NULL';
