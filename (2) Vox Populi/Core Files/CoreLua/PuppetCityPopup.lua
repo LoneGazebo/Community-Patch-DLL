6b1cf3198f2ff8621b60dc9249985bb6218574f0
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2022-05-02 15:29:33 -0400

    Fix a typo

--- (2) Vox Populi/Core Files/CoreLua/PuppetCityPopup.lua
+++ (2) Vox Populi/Core Files/CoreLua/PuppetCityPopup.lua
@@ -38,22 +38,7 @@ PopupLayouts[ButtonPopupTypes.BUTTONPOPUP_CITY_CAPTURED] = function(popupInfo)
 	popupText = popupText .. "  " .. Locale.ConvertTextKey("TXT_KEY_POPUP_CITY_CAPTURE_INFO");
 	
 	SetPopupText(popupText);
-	
-	-- Calculate Happiness info
---	local iUnhappinessNoCity = activePlayer:GetUnhappiness();
---	local iUnhappinessAnnexedCity = activePlayer:GetUnhappinessForecast(newCity, nil);	-- pAssumeCityAnnexed, pAssumeCityPuppeted
---	if (bMinorCivBuyout) then
-		-- For minor civ buyout (Austria UA), there are no unhappiness benefits of annexing because the city is not occupied
---		iUnhappinessAnnexedCity = activePlayer:GetUnhappinessForecast(nil, newCity);
---	end
-	--local iUnhappinessPuppetCity = activePlayer:GetUnhappinessForecast(nil, newCity);		-- pAssumeCityAnnexed, pAssumeCityPuppeted
-	
---CBP
-	--local iUnhappinessForAnnexing = (newCity:GetPopulation() / 2);
-	--local iUnhappinessForPuppeting = (newCity:GetPopulation() / 2);
-	--local iUnhappinessForAnnexing = iUnhappinessAnnexedCity - iUnhappinessNoCity;
-	--local iUnhappinessForPuppeting = iUnhappinessPuppetCity - iUnhappinessNoCity;
---END
+
 	-- Initialize 'Liberate' button.
 	if (iLiberatedPlayer ~= -1) then
 		local OnLiberateClicked = function()
@@ -110,7 +95,7 @@ PopupLayouts[ButtonPopupTypes.BUTTONPOPUP_CITY_CAPTURED] = function(popupInfo)
 
 		if (not bOneCity) then
 			buttonText = Locale.ConvertTextKey("TXT_KEY_POPUP_RAZE_CAPTURED_CITY");
-			strToolTip = Locale.ConvertTextKey("TXT_KEY_POPUP_CITY_CAPTURE_INFO_RAZE", iUnhappinessForAnnexing);
+			strToolTip = Locale.ConvertTextKey("TXT_KEY_POPUP_CITY_CAPTURE_INFO_RAZE");
 		else
 			buttonText = Locale.ConvertTextKey("TXT_KEY_POPUP_DESTROY_CAPTURED_CITY");
 			strTooltip = Locale.ConvertTextKey("TXT_KEY_POPUP_CITY_CAPTURE_INFO_DESTROY");
