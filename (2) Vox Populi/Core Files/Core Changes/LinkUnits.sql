57622424e3f384624d8950f042dd2dd2086e2c22
nobubblegums <94949778+nobubblegums@users.noreply.github.com>
2022-08-07 19:23:55 +0300

    Linked&Group movement turned into a modmod (#9087)
    I ironed out the bugs but I won't have the time or motivation to support this if issues arise, so making it a modmod now, sorry for the mess. It's not like the community agreed on this either.

--- (2) Vox Populi/Core Files/Core Changes/LinkUnits.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO Language_en_US (Tag, Text) VALUES 
-('TXT_KEY_ACTION_LINK_UNITS', "Link units on this plot."),
-('TXT_KEY_ACTION_UNLINK_UNITS', "Unlink units.");
-
-
-UPDATE InterfaceModes SET Visible = 1, HotKey = "KB_X", AltDown = 0, SelectAll = 0, IconIndex = 0, IconAtlas = "GROUP_MOVE_ATLAS" WHERE Type = "INTERFACEMODE_MOVE_TO_ALL";
-UPDATE Language_en_US SET Text = "Group Move" WHERE Tag = "TXT_KEY_INTERFACEMODE_MOVE_TO_ALL";
-UPDATE Language_en_US SET Text = "Order the selected unit and adjacent combat units of the same domain to move." WHERE Tag = "TXT_KEY_INTERFACEMODE_MOVE_TO_ALL_HELP";
--- CvGameCoreDLL_Expansion2/CvUnitMission.cpp
+++ CvGameCoreDLL_Expansion2/CvUnitMission.cpp
@@ -543,7 +543,7 @@ void CvUnitMission::ContinueMission(CvUnit* hUnit, int iSteps)
 						else //cannot move or no need to move
 						{
 							bDone = true;
-							if (hUnit->IsGrouped()) {
+							if (MOD_LINKED_MOVEMENT && hUnit->IsGrouped()) {
 								hUnit->SetIsGrouped(false);
 							}
 						}
