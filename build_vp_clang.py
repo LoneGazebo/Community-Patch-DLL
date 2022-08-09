f290a4b06557d04059000293e321b67ca5fb158b
Ian <7848638+IanE9@users.noreply.github.com>
2022-08-09 17:09:24 -0600

    enable SSE3 by default in clang builds Steam's hardware survey says 100% users support the instruction set

--- build_vp_clang.py
+++ build_vp_clang.py
@@ -301,7 +301,7 @@ class TaskMan:
         return results
 
 def build_cl_config_args(config: Config) -> list[str]:
-    args = ['-m32', '/c', '/MD', '/GS', '/EHsc', '/fp:precise', '/Zc:wchar_t', '/Z7']
+    args = ['-m32', '-msse3', '/c', '/MD', '/GS', '/EHsc', '/fp:precise', '/Zc:wchar_t', '/Z7']
     if config == Config.Release:
         args.append('/Ox')
         args.append('/Ob2')
