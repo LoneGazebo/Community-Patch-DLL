b8b2feaaa877a3365edfae8bbaa9f5a4886f35cd
LessRekkless <34615525+LessRekkless@users.noreply.github.com>
2022-08-12 18:42:13 -0400

    fix warnings from database data type conversion into new data type in dll.

--- (1) Community Patch/Core Files/PNM Mods DB/PROMOTIONS/HalfMove.sql
+++ (1) Community Patch/Core Files/PNM Mods DB/PROMOTIONS/HalfMove.sql
@@ -1,14 +1,14 @@
 -- unit movement cost is doubled in the given terrain / feature
 ALTER TABLE UnitPromotions_Terrains
-  ADD HalfMove INTEGER DEFAULT 0;
+  ADD HalfMove boolean DEFAULT 0;
 ALTER TABLE UnitPromotions_Features
-  ADD HalfMove INTEGER DEFAULT 0;
+  ADD HalfMove boolean DEFAULT 0;
 
 -- unit movement cost is increased by 1 in the given terrain / feature
 ALTER TABLE UnitPromotions_Terrains
-  ADD ExtraMove INTEGER DEFAULT 0;
+  ADD ExtraMove boolean DEFAULT 0;
 ALTER TABLE UnitPromotions_Features
-  ADD ExtraMove INTEGER DEFAULT 0;
+  ADD ExtraMove boolean DEFAULT 0;
 
 INSERT INTO CustomModDbUpdates(Name, Value) VALUES('PROMOTIONS_HALF_MOVE', 1);
 
