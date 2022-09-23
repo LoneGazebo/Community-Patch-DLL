94cff6dd3b094f88e0eb55d0e4e5fd070be0a680
Adam <33016300+adaneslavo@users.noreply.github.com>
2022-09-23 20:24:53 +0200

    Update AssignStartingPlots.lua

--- (2) Vox Populi/Core Files/CoreLua/AssignStartingPlots.lua
+++ (2) Vox Populi/Core Files/CoreLua/AssignStartingPlots.lua
@@ -6459,7 +6459,7 @@ function AssignStartingPlots:GenerateNaturalWondersCandidatePlotLists(target_num
 	self.wonder_list = table.fill(-1, self.iNumNW);
 	local next_wonder_number = 1;
 	for row in GameInfo.Features() do
-		if (row.NaturalWonder == true or row.PseudoNaturalWonder == 1) then -- adan_eslavo
+		if (row.NaturalWonder == true or row.PseudoNaturalWonder == 1) then
 			self.wonder_list[next_wonder_number] = row.Type;
 			next_wonder_number = next_wonder_number + 1;
 		end
