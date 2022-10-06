f97c15dd7db9baa4229fbcdf62272cb0af82f7fc
pineappledan <33136027+pineappledan@users.noreply.github.com>
2022-10-06 17:45:18 -0600

    Building_YieldModifiers cleanup
    BuildingChanges.sql deletes all the Yield Modifiers so entries in other files risked being deleted
    I took every instance of Building_YieldModifiers and consolidated them into 1 file
    I also moved all culture yield modifiers into this table, and removed all instances of CultureRateModifier (dedicated Buildings column)
    Culture bonuses that use this column are not exposed to the UI for the civilopedia and building text popup

--- (2) Vox Populi/Balance Changes/Wonders/WonderChanges.sql
+++ (2) Vox Populi/Balance Changes/Wonders/WonderChanges.sql
@@ -183,7 +183,7 @@ VALUES ('BUILDING_ANGKOR_WAT' , 'YIELD_FAITH' , '1');
 -- Alhambra
 
 UPDATE Buildings
-SET PolicyType = 'POLICY_HONOR_FINISHER', Cost = '400', CultureRateModifier = '10', FreePromotion = 'PROMOTION_ALHAMBRA', TrainedFreePromotion = NULL
+SET PolicyType = 'POLICY_HONOR_FINISHER', Cost = '400', CultureRateModifier = '0', FreePromotion = 'PROMOTION_ALHAMBRA', TrainedFreePromotion = NULL
 WHERE Type = 'BUILDING_ALHAMBRA';
 
 -- Mosque of Djenne
@@ -455,7 +455,7 @@ VALUES
 
 -- Sydney Opera House
 UPDATE Buildings
-SET PrereqTech = 'TECH_SATELLITES', Cost = '2300', CultureRateModifier = '25', MinAreaSize = '10'
+SET PrereqTech = 'TECH_SATELLITES', Cost = '2300', CultureRateModifier = '0', MinAreaSize = '10'
 WHERE Type = 'BUILDING_SYDNEY_OPERA_HOUSE';
 
 -- Move Apollo 
