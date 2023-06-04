b3b9d114e061596dc4c2a5d9acd4019aa60c7df8
azum4roll <rl8263@gmail.com>
2023-06-04 18:38:32 +0800

    block CS from spawning adjacent to ice this means no more CS-exclusive 2-tile polar seas

--- (2) Vox Populi/Core Files/CoreLua/AssignStartingPlots.lua
+++ (2) Vox Populi/Core Files/CoreLua/AssignStartingPlots.lua
@@ -182,7 +182,7 @@ function AssignStartingPlots.Create()
 		AttemptToPlaceSmallStrategicAtPlot = AssignStartingPlots.AttemptToPlaceSmallStrategicAtPlot,
 		FindFallbackForUnmatchedRegionPriority = AssignStartingPlots.FindFallbackForUnmatchedRegionPriority,
 		AddStrategicBalanceResources = AssignStartingPlots.AddStrategicBalanceResources,
-		AttemptToPlaceForestAtGrassPlot = AssignStartingPlots.AttemptToPlaceForestAtGrassPlot,
+		AttemptToPlaceCattleAtGrassPlot = AssignStartingPlots.AttemptToPlaceCattleAtGrassPlot,
 		AttemptToPlaceStoneAtGrassPlot = AssignStartingPlots.AttemptToPlaceStoneAtGrassPlot,
 		NormalizeStartLocation = AssignStartingPlots.NormalizeStartLocation,
 		NormalizeTeamLocations = AssignStartingPlots.NormalizeTeamLocations,
@@ -4228,32 +4228,33 @@ function AssignStartingPlots:AddStrategicBalanceResources(region_number)
 
 	if table.maxn(iron_list) > 0 then
 		shuf_list = GetShuffledCopyOfTable(iron_list)
-		iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.iron_ID, iron_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
+		local iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.iron_ID, iron_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
 		if iNumLeftToPlace == 0 then
 			placed_iron = true;
 		end
 	end
 	if table.maxn(horse_list) > 0 then
 		shuf_list = GetShuffledCopyOfTable(horse_list)
-		iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.horse_ID, horse_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
+		local iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.horse_ID, horse_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
 		if iNumLeftToPlace == 0 then
 			placed_horse = true;
 		end
 	end
 	if placed_iron == false and table.maxn(iron_fallback) > 0 then
 		shuf_list = GetShuffledCopyOfTable(iron_fallback)
-		iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.iron_ID, iron_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
+		local iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.iron_ID, iron_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
 	end
 	if placed_horse == false and table.maxn(horse_fallback) > 0 then
 		shuf_list = GetShuffledCopyOfTable(horse_fallback)
-		iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.horse_ID, horse_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
+		local iNumLeftToPlace = self:PlaceSpecificNumberOfResources(self.horse_ID, horse_amt, 1, 1, ImpactLayers.LAYER_NONE, 0, 0, shuf_list);
 	end
 end
 ------------------------------------------------------------------------------
-function AssignStartingPlots:AttemptToPlaceForestAtGrassPlot(x, y)
+function AssignStartingPlots:AttemptToPlaceCattleAtGrassPlot(x, y)
 	-- Function modified May 2011 to boost production at heavy grass starts. - BT
 	-- Now placing Stone instead of Cows. Returns true if Stone is placed.
 	-- Now placing Forest instead of Stone. -- September 2020, azum4roll
+	-- Now placing Cattle instead of Forest. We've gone full circle! -- June 2023, azum4roll
 	--print("-"); print("Attempting to place Forest at: ", x, y);
 	local plot = Map.GetPlot(x, y);
 	if plot == nil then
@@ -4269,9 +4270,9 @@ function AssignStartingPlots:AttemptToPlaceForestAtGrassPlot(x, y)
 		local featureType = plot:GetFeatureType()
 		if featureType == FeatureTypes.NO_FEATURE then
 			local terrainType = plot:GetTerrainType()
-			if terrainType == TerrainTypes.TERRAIN_GRASS then -- Place Forest
-				plot:SetFeatureType(FeatureTypes.FEATURE_FOREST, -1);
-				--print("Placed Forest.");
+			if terrainType == TerrainTypes.TERRAIN_GRASS then -- Place Cattle
+				plot:SetResourceType(self.cow_ID, -1);
+				--print("Placed Cattle.");
 				return true
 			end
 		end
@@ -4526,7 +4527,6 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 
 	for loop, plot_adjustments in ipairs(search_table) do
 		local searchX, searchY = self:ApplyHexAdjustment(x, y, plot_adjustments)
-		local plot = Map.GetPlot(x, y);
 		--
 		--
 		if searchX < 0 or searchX >= iW or searchY < 0 or searchY >= iH then
@@ -4668,6 +4668,7 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 	local innerHammerScore = (4 * innerHills) + (2 * innerForest) + innerOneHammer;
 	local outerHammerScore = (2 * outerHills) + outerForest + outerOneHammer;
 	local earlyHammerScore = (2 * innerForest) + outerForest + innerOneHammer + outerOneHammer;
+	local randomized_first_ring_adjustments, randomized_second_ring_adjustments, randomized_third_ring_adjustments;
 	-- If drastic shortage, attempt to add a hill to first ring.
 	if (outerHammerScore < 8 and innerHammerScore < 2) or innerHammerScore == 0 then -- Change a first ring plot to Hills.
 		if isEvenY then
@@ -4733,7 +4734,6 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 			local plot_adjustments = randomized_second_ring_adjustments[attempt];
 			local searchX, searchY = self:ApplyHexAdjustment(x, y, plot_adjustments)
 			-- Attempt to place a Forest at the currently chosen plot.
-			local plot = Map.GetPlot(searchX, searchY);
 			if plot:GetResourceType(-1) == -1 then -- No resource here, safe to proceed.
 				local plotType = plot:GetPlotType();
 				local terrainType = plot:GetTerrainType();
@@ -4796,6 +4796,7 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 		local currentY = y;
 		for direction_index = 1, 6 do
 			for plot_to_handle = 1, ripple_radius do
+				local plot_adjustments, nextX, nextY;
 				if currentY / 2 > math.floor(currentY / 2) then
 					plot_adjustments = odd[direction_index];
 				else
@@ -4859,7 +4860,6 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 		print("-");
 
 		local innerPlaced, outerPlaced = 0, 0;
-		local randomized_first_ring_adjustments, randomized_second_ring_adjustments, randomized_third_ring_adjustments;
 		if isEvenY then
 			randomized_first_ring_adjustments = GetShuffledCopyOfTable(self.firstRingYIsEven);
 			randomized_second_ring_adjustments = GetShuffledCopyOfTable(self.secondRingYIsEven);
@@ -4873,7 +4873,6 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 		local tried_all_second_ring = false;
 		local tried_all_third_ring = false;
 		local allow_oasis = true; -- Permanent flag. (We don't want to place more than one Oasis per location).
-		local placedOasis; -- Records returning result from each attempt.
 		while iNumFoodBonusNeeded > 0 do
 			if ((innerPlaced < 2 and innerCanHaveBonus > 0) or (self.legStart and innerPlaced < 3 and innerCanHaveBonus > 0))
 			  and tried_all_first_ring == false then
@@ -4937,15 +4936,15 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 						tried_all_third_ring = true;
 					end
 				end
-				
 			else -- Tried everywhere, have to give up.
-				break				
+				break
 			end
 		end
 	end
 
 	-- Check for heavy grass and light plains. Adding Stone if grass count is high and plains count is low. - May 2011, BT
 	-- Changed to check for only FLAT grass/plains and add forests instead. - September 2020, azum4roll
+	-- Add Cattle instead. - June 2023, azum4roll
 	local iNumStoneNeeded = 0;
 	if iNumGrass >= 9 and iNumPlains == 0 then
 		iNumStoneNeeded = 2;
@@ -4954,7 +4953,6 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 	end
 	if iNumStoneNeeded > 0 then -- Add Forest to this grass start.
 		local stonePlaced, innerPlaced = 0, 0;
-		local randomized_first_ring_adjustments, randomized_second_ring_adjustments;
 		if isEvenY then
 			randomized_first_ring_adjustments = GetShuffledCopyOfTable(self.firstRingYIsEven);
 			randomized_second_ring_adjustments = GetShuffledCopyOfTable(self.secondRingYIsEven);
@@ -4970,10 +4968,10 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 				for attempt = 1, 6 do
 					local plot_adjustments = randomized_first_ring_adjustments[attempt];
 					local searchX, searchY = self:ApplyHexAdjustment(x, y, plot_adjustments)
-					-- Attempt to place Forest at the currently chosen plot.
-					local placedBonus = self:AttemptToPlaceForestAtGrassPlot(searchX, searchY);
+					-- Attempt to place Cattle at the currently chosen plot.
+					local placedBonus = self:AttemptToPlaceCattleAtGrassPlot(searchX, searchY);
 					if placedBonus == true then
-						print("Placed Forest in first ring at ", searchX, searchY);
+						print("Placed Cattle in first ring at ", searchX, searchY);
 						innerPlaced = innerPlaced + 1;
 						iNumStoneNeeded = iNumStoneNeeded - 1;
 						break
@@ -4987,10 +4985,10 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 				for attempt = 1, 12 do
 					local plot_adjustments = randomized_second_ring_adjustments[attempt];
 					local searchX, searchY = self:ApplyHexAdjustment(x, y, plot_adjustments)
-					-- Attempt to place Forest at the currently chosen plot.
-					local placedBonus = self:AttemptToPlaceForestAtGrassPlot(searchX, searchY);
+					-- Attempt to place Cattle at the currently chosen plot.
+					local placedBonus = self:AttemptToPlaceCattleAtGrassPlot(searchX, searchY);
 					if placedBonus == true then
-						print("Placed Forest in second ring at ", searchX, searchY);
+						print("Placed Cattle in second ring at ", searchX, searchY);
 						iNumStoneNeeded = iNumStoneNeeded - 1;
 						break
 					elseif attempt == 12 then
@@ -5003,7 +5001,7 @@ function AssignStartingPlots:NormalizeStartLocation(region_number)
 			end
 		end
 	end
-	
+
 	-- Record conditions at this start location.
 	local results_table = {alongOcean, nextToLake, isRiver, nearRiver, nearMountain, forestCount, jungleCount, tundraCount, desertCount};
 	self.startLocationConditions[region_number] = results_table;
@@ -6872,7 +6870,7 @@ function AssignStartingPlots:AssignCityStatesToRegionsOrToUninhabited(args)
 				local plot = Map.GetPlot(x, y);
 				local plotType = plot:GetPlotType()
 				local terrainType = plot:GetTerrainType()
-				if (plotType == PlotTypes.PLOT_LAND or plotType == PlotTypes.PLOT_HILLS) and terrainType ~= TerrainTypes.TERRAIN_SNOW then -- Habitable land plot, process it.
+				if (plotType == PlotTypes.PLOT_LAND or plotType == PlotTypes.PLOT_HILLS) and self:CanPlaceCityStateAt(x, y, plot:GetArea(), false, false) then -- Habitable land plot, process it.
 					local iArea = plot:GetArea();
 					if self.method == RegionDivision.RECTANGULAR_SELF_DEFINED then -- Determine if plot is inside or outside the regional rectangle
 						if (x >= self.inhabited_WestX and x <= self.inhabited_WestX + self.inhabited_Width - 1) and
@@ -6908,7 +6906,7 @@ function AssignStartingPlots:AssignCityStatesToRegionsOrToUninhabited(args)
 					iNumCivLandmassPlots = iNumCivLandmassPlots + plot_count;
 				else
 					iNumUninhabitedLandmassPlots = iNumUninhabitedLandmassPlots + plot_count;
-					if plot_count < 4 then
+					if plot_count < 3 then
 						table.insert(areas_too_small, areaID);
 					else
 						table.insert(areas_uninhabited, areaID);
@@ -6923,7 +6921,7 @@ function AssignStartingPlots:AssignCityStatesToRegionsOrToUninhabited(args)
 						local y = (plotIndex - x - 1) / iW;
 						local plot = Map.GetPlot(x, y);
 						local terrainType = plot:GetTerrainType();
-						if terrainType ~= TerrainTypes.TERRAIN_SNOW then
+						if self:CanPlaceCityStateAt(x, y, areaID, false, false) then
 							if self.plotDataIsCoastal[plotIndex] == true then
 								table.insert(self.uninhabited_areas_coastal_plots, plotIndex);
 							else
@@ -7049,6 +7047,13 @@ function AssignStartingPlots:CanPlaceCityStateAt(x, y, area_ID, force_it, ignore
 		return false
 	end
 
+	-- Avoid ice
+	for nearPlot in self:Plot_GetPlotsInCircle(plot, 1) do
+		if nearPlot:GetFeatureType() == FeatureTypes.FEATURE_ICE then
+			return false
+		end
+	end
+
 	-- Avoid natural wonders
 	for nearPlot in self:Plot_GetPlotsInCircle(plot, 1, 4) do
 		local featureInfo = GameInfo.Features[nearPlot:GetFeatureType()]
@@ -7060,7 +7065,7 @@ function AssignStartingPlots:CanPlaceCityStateAt(x, y, area_ID, force_it, ignore
 	
 	-- Reserve the best city sites for major civs
 	local fertility = self:Plot_GetFertilityInRange(plot, 2)
-	if fertility > 28 then
+	if fertility > 30 then
 		--print("CanPlaceCityStateAt: avoided fertility: ", fertility)
 		return false
 	end
@@ -7069,7 +7074,7 @@ function AssignStartingPlots:CanPlaceCityStateAt(x, y, area_ID, force_it, ignore
 	if self.impactData[ImpactLayers.LAYER_CITY_STATE][plotIndex] > 0 and force_it == false then
 		return false
 	end
-	local plotIndex = y * iW + x + 1;
+	plotIndex = y * iW + x + 1;
 	if self.playerCollisionData[plotIndex] == true and ignore_collisions == false then
 		print("-"); print("City State candidate plot rejected: collided with already-placed civ or City State at", x, y);
 		return false
