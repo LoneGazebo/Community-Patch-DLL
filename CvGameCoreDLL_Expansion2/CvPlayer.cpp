3be088b102fa33d7ba273b54c1a46c53fbdc82b3
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2023-08-12 17:07:18 -0400

    Make (5-46) amount customizable

--- CvGameCoreDLL_Expansion2/CvPlayer.cpp
+++ CvGameCoreDLL_Expansion2/CvPlayer.cpp
@@ -9904,7 +9904,7 @@ void CvPlayer::DoLiberatePlayer(PlayerTypes ePlayer, int iOldCityID, bool bForce
 
 		// If liberating a City-State, increase Influence resting point
 		if (GET_PLAYER(ePlayer).isMinorCiv())
-			GET_PLAYER(ePlayer).GetMinorCivAI()->ChangeRestingPointChange(GetID(), 100);
+			GET_PLAYER(ePlayer).GetMinorCivAI()->ChangeRestingPointChange(GetID(), /*0 in CP, 100 in VP*/ GD_INT_GET(MINOR_LIBERATION_RESTING_INFLUENCE));
 	}
 
 	// Bonuses for liberation OR sphere removal
