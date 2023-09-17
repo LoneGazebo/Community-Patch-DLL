LoneGazebo/Community-Patch-DLL

commit 8dc1fb216fbc63bfe5c1ab2187602ad085c06503

Turn speed small improvements (#10186)
- Implementing `CvCityBuildings::CacheBuildingTypeByClass`, so `CvCityBuildings::GetBuildingTypeFromClass`
works faster.
- `CvUnitMovement::GetCostsForMove` minor tweaks.
- Also some small improvements like `inline` on frequently called methods.

CvGameCoreDLL_Expansion2/CvBuildingClasses.cpp
@@ -4401,7 +4401,7 @@ std::vector<CvBuildingEntry*>& CvBuildingXMLEntries::GetBuildingEntries()
 }
 
 /// Number of defined policies
-int CvBuildingXMLEntries::GetNumBuildings()
+inline int CvBuildingXMLEntries::GetNumBuildings()
 {
 	return m_paBuildingEntries.size();
 }
@@ -4509,6 +4509,32 @@ void CvCityBuildings::Init(CvBuildingXMLEntries* pPossibleBuildings, CvCity* pCi
 	Reset();
 }
 
+void CvCityBuildings::SetBuildingTypeByClassDirty(bool dirty) const {
+	b_existBuildingsAreDirty = dirty;
+}
+
+void CvCityBuildings::CacheBuildingTypeByClass() const {
+	if (!b_existBuildingsAreDirty) {
+		return;
+	}
+
+	m_buildingTypeByClass.clear();
+
+	for (int i = 0; i < GC.getNumBuildingClassInfos(); i++)
+	{
+		for (std::vector<BuildingTypes>::const_iterator iI = m_buildingsThatExistAtLeastOnce.begin(); iI != m_buildingsThatExistAtLeastOnce.end(); ++iI)
+		{
+			CvBuildingEntry* pkInfo = GC.getBuildingInfo(*iI);
+			if (pkInfo && pkInfo->GetBuildingClassType() == i && GetNumBuilding(*iI) > 0)
+			{
+				m_buildingTypeByClass.insert(std::make_pair((BuildingClassTypes) i, (BuildingTypes) *iI));
+			}
+		}
+	}
+
+	SetBuildingTypeByClassDirty(false);
+}
+
 /// Deallocate memory created in initialize
 void CvCityBuildings::Uninit()
 {
@@ -4557,6 +4583,7 @@ void CvCityBuildings::Reset()
 
 #if defined(MOD_BALANCE_CORE)
 	m_buildingsThatExistAtLeastOnce.clear();
+	m_buildingTypeByClass.clear();
 #endif
 
 }
@@ -4600,6 +4627,7 @@ void CvCityBuildings::Read(FDataStream& kStream)
 	for (int i=0; i<m_pPossibleBuildings->GetNumBuildings(); i++)
 		if (m_paiNumRealBuilding[i]>0 || m_paiNumFreeBuilding[i]>0)
 			m_buildingsThatExistAtLeastOnce.push_back( (BuildingTypes)i );
+	SetBuildingTypeByClassDirty(true);
 
 }
 
@@ -4698,14 +4726,8 @@ BuildingTypes CvCityBuildings::GetBuildingTypeFromClass(BuildingClassTypes eInde
 {
 	CvAssertMsg(eIndex != NO_BUILDINGCLASS, "BuildingClassTypes eIndex is expected to not be NO_BUILDINGCLASS");
 
-	for (std::vector<BuildingTypes>::const_iterator iI = m_buildingsThatExistAtLeastOnce.begin(); iI != m_buildingsThatExistAtLeastOnce.end(); ++iI)
-	{
-		CvBuildingEntry* pkInfo = GC.getBuildingInfo(*iI);
-		if (pkInfo && pkInfo->GetBuildingClassType() == eIndex && GetNumBuilding(*iI) > 0)
-		{
-			return *iI;
-		}
-	}
+	CacheBuildingTypeByClass();
+	m_buildingTypeByClass.find(eIndex);
 	return NO_BUILDING;
 }
 
@@ -5094,6 +5116,7 @@ void CvCityBuildings::SetNumRealBuildingTimed(BuildingTypes eIndex, int iNewValu
 		{
 			if ( std::find( m_buildingsThatExistAtLeastOnce.begin(), m_buildingsThatExistAtLeastOnce.end(), eIndex ) == m_buildingsThatExistAtLeastOnce.end() )
 				m_buildingsThatExistAtLeastOnce.push_back(eIndex);
+			SetBuildingTypeByClassDirty(true);
 		}
 		else if (GetNumFreeBuilding(eIndex)==0)
 		{
@@ -5101,6 +5124,7 @@ void CvCityBuildings::SetNumRealBuildingTimed(BuildingTypes eIndex, int iNewValu
 			std::vector<BuildingTypes>::iterator pos = std::find( m_buildingsThatExistAtLeastOnce.begin(), m_buildingsThatExistAtLeastOnce.end(), eIndex );
 			if ( pos != m_buildingsThatExistAtLeastOnce.end() )
 				m_buildingsThatExistAtLeastOnce.erase(pos);
+			SetBuildingTypeByClassDirty(true);
 		}
 #endif
 
@@ -5345,6 +5369,7 @@ void CvCityBuildings::SetNumFreeBuilding(BuildingTypes eIndex, int iNewValue)
 		{
 			if ( std::find( m_buildingsThatExistAtLeastOnce.begin(), m_buildingsThatExistAtLeastOnce.end(), eIndex ) == m_buildingsThatExistAtLeastOnce.end() )
 				m_buildingsThatExistAtLeastOnce.push_back(eIndex);
+			SetBuildingTypeByClassDirty(true);
 		}
 		else if (GetNumRealBuilding(eIndex)==0)
 		{
@@ -5352,6 +5377,7 @@ void CvCityBuildings::SetNumFreeBuilding(BuildingTypes eIndex, int iNewValue)
 			std::vector<BuildingTypes>::iterator pos = std::find( m_buildingsThatExistAtLeastOnce.begin(), m_buildingsThatExistAtLeastOnce.end(), eIndex );
 			if ( pos != m_buildingsThatExistAtLeastOnce.end() )
 				m_buildingsThatExistAtLeastOnce.erase(pos);
+			SetBuildingTypeByClassDirty(true);
 		}
 
 		// This condensed logic comes from SetNumRealBuilding()

CvGameCoreDLL_Expansion2/CvBuildingClasses.h
@@ -1117,6 +1117,9 @@ public:
 	void Read(FDataStream& kStream);
 	void Write(FDataStream& kStream) const;
 
+	void SetBuildingTypeByClassDirty(bool dirty) const;
+	void CacheBuildingTypeByClass() const;
+
 	// Accessor functions
 	CvBuildingXMLEntries* GetPossibleBuildings() const;
 
@@ -1262,6 +1265,9 @@ private:
 
 #if defined(MOD_BALANCE_CORE)
 	std::vector<BuildingTypes> m_buildingsThatExistAtLeastOnce;
+
+	mutable bool b_existBuildingsAreDirty;
+	mutable std::map<BuildingClassTypes, BuildingTypes> m_buildingTypeByClass;
 #endif
 
 	std::vector<BuildingYieldChange> m_aBuildingYieldChange;

CvGameCoreDLL_Expansion2/CvGlobals.cpp
@@ -3814,7 +3814,7 @@ CvBuildingClassInfo* CvGlobals::getBuildingClassInfo(BuildingClassTypes eBuildin
 		return NULL;
 }
 
-int CvGlobals::getNumBuildingInfos()
+inline int CvGlobals::getNumBuildingInfos()
 {
 	return m_pBuildings->GetNumBuildings();
 }
@@ -3824,7 +3824,7 @@ std::vector<CvBuildingEntry*>& CvGlobals::getBuildingInfo()
 	return m_pBuildings->GetBuildingEntries();
 }
 
-CvBuildingEntry* CvGlobals::getBuildingInfo(BuildingTypes eBuildingNum)
+inline CvBuildingEntry* CvGlobals::getBuildingInfo(BuildingTypes eBuildingNum)
 {
 	CvAssert(eBuildingNum > -1);
 	CvAssert(eBuildingNum < GC.getNumBuildingInfos());

CvGameCoreDLL_Expansion2/CvPlayer.cpp
@@ -10827,7 +10827,7 @@ void CvPlayer::SetDangerPlotsDirty()
 }
 
 //	--------------------------------------------------------------------------------
-bool CvPlayer::isHuman() const
+inline bool CvPlayer::isHuman() const
 {
 	if(GetID() == NO_PLAYER)
 	{

CvGameCoreDLL_Expansion2/CvPlayerAI.cpp
@@ -48,7 +48,7 @@
 
 static CvEnumMap<PlayerTypes, CvPlayerAI> s_players;
 
-CvPlayerAI& CvPlayerAI::getPlayer(PlayerTypes ePlayer)
+inline CvPlayerAI& CvPlayerAI::getPlayer(PlayerTypes ePlayer)
 {
 	CvAssertMsg(ePlayer != NO_PLAYER, "Player is not assigned a valid value");
 	CvAssertMsg(ePlayer < MAX_PLAYERS, "Player is not assigned a valid value");

CvGameCoreDLL_Expansion2/CvPlot.cpp
@@ -13570,7 +13570,7 @@ int CvPlot::countNumAirUnits(TeamTypes eTeam, bool bNoSuicide) const
 }
 
 //	--------------------------------------------------------------------------------
-int CvPlot::GetPlotIndex() const
+inline int CvPlot::GetPlotIndex() const
 {
 	return m_iPlotIndex;
 }

CvGameCoreDLL_Expansion2/CvPlot.h
@@ -408,7 +408,7 @@ public:
 	{
 		return (PlotTypes)m_ePlotType;
 	}
-	bool isWater()          const
+	inline bool isWater()          const
 	{
 		return (PlotTypes)m_ePlotType == PLOT_OCEAN;
 	};

CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -18629,12 +18629,12 @@ void CvUnit::ChangeIsConvertUnit(int iValue)
 	VALIDATE_OBJECT
 	m_iConvertUnit += iValue;
 }
-int CvUnit::getIsConvertUnit() const
+inline int CvUnit::getIsConvertUnit() const
 {
 	VALIDATE_OBJECT
 	return	m_iConvertUnit;
 }
-bool CvUnit::isConvertUnit() const
+inline bool CvUnit::isConvertUnit() const
 {
 	VALIDATE_OBJECT
 	return getIsConvertUnit() > 0;

commit 52638149bb1939df69af34f5aa618708190fe636

Fixing bug introduced in #10186
Fixes #10208, #10209

CvGameCoreDLL_Expansion2/CvBuildingClasses.cpp
@@ -4728,8 +4728,13 @@ BuildingTypes CvCityBuildings::GetBuildingTypeFromClass(BuildingClassTypes eInde
 	CvAssertMsg(eIndex != NO_BUILDINGCLASS, "BuildingClassTypes eIndex is expected to not be NO_BUILDINGCLASS");
 
 	CacheBuildingTypeByClass();
-	m_buildingTypeByClass.find(eIndex);
-	return NO_BUILDING;
+	std::map<BuildingClassTypes, BuildingTypes>::iterator it = m_buildingTypeByClass.find(eIndex);
+	if (it != m_buildingTypeByClass.end()) {
+		return it->second;
+	}
+	else {
+		return NO_BUILDING;
+	}
 }
 
 /// Accessor: Removes all real buildings which belong to the specified building class.