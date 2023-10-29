1f27b61b5040546f1a8edfe538167cc8157d3f1d
azum4roll <rl8263@gmail.com>
2023-10-29 21:49:19 +0800

    cleanup

--- (2) Vox Populi/Database Changes/Triggers.sql
+++ (2) Vox Populi/Database Changes/Triggers.sql
@@ -9,12 +9,12 @@ CREATE TRIGGER Unit_BuildingClassPurchaseRequireds
 AFTER INSERT ON Units
 WHEN (EXISTS (SELECT Type FROM Units WHERE Type = NEW.Type))
 BEGIN
-INSERT INTO Unit_BuildingClassPurchaseRequireds
-	(UnitType, BuildingClassType)
-SELECT
-	NEW.Type, BuildingClassType
-FROM Unit_BuildingClassPurchaseRequireds
-WHERE UnitType IN (SELECT DefaultUnit FROM UnitClasses WHERE Type = NEW.Class);
+	INSERT INTO Unit_BuildingClassPurchaseRequireds
+		(UnitType, BuildingClassType)
+	SELECT
+		NEW.Type, BuildingClassType
+	FROM Unit_BuildingClassPurchaseRequireds
+	WHERE UnitType IN (SELECT DefaultUnit FROM UnitClasses WHERE Type = NEW.Class);
 END;
 
 ---------------------------------------------------------------------------------
