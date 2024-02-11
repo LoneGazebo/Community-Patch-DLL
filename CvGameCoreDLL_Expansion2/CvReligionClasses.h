071eb74ddf7c2e8322b2aeee6244196c65e4d9f9
axatin <95587882+axatin@users.noreply.github.com>
2024-02-11 23:09:09 +0100

    Rework of AI Pantheon Selection

--- CvGameCoreDLL_Expansion2/CvReligionClasses.h
+++ CvGameCoreDLL_Expansion2/CvReligionClasses.h
@@ -582,7 +582,8 @@ public:
 	CvCity *ChooseProphetConversionCity(CvUnit* pUnit = NULL, int* piTurns = NULL) const;
 	ReligionTypes GetReligionToSpread(bool bConsiderForeign) const;
 	ReligionTypes GetFavoriteForeignReligion(bool bForInternalSpread) const;
-	int ScoreBelief(CvBeliefEntry* pEntry, bool bForBonus = false, bool bConsiderFutureTech = true) const;
+	CvWeightedVector<int> CalculatePlotWeightsForBeliefSelection(bool bConsiderExpansion) const;
+	int ScoreBelief(CvBeliefEntry* pEntry, CvWeightedVector<int> viPlotWeights, bool bForBonus = false, bool bConsiderFutureTech = true) const;
 
 private:
 #if defined(MOD_BALANCE_CORE)
@@ -603,9 +604,13 @@ private:
 	bool BuyAnyAvailableFaithBuilding();
 
 	int ScoreBeliefAtPlot(CvBeliefEntry* pEntry, CvPlot* pPlot, bool bConsiderFutureTech) const;
+	int ScorePantheonBeliefAtCity(CvBeliefEntry* pEntry, CvCity* pCity) const;
 	int ScoreBeliefAtCity(CvBeliefEntry* pEntry, CvCity* pCity) const;
 	int ScoreBeliefForPlayer(CvBeliefEntry* pEntry, bool bReturnConquest = false, bool bReturnCulture = false, bool bReturnScience = false, bool bReturnDiplo = false) const;
-	int GetValidPlotYield(CvBeliefEntry* pEntry, CvPlot* pPlot, YieldTypes iI, bool bConsiderFutureTech) const;
+	int ScorePantheonBeliefForPlayer(CvBeliefEntry* pEntry) const;
+	int GetValidPlotYieldTimes100(CvBeliefEntry* pEntry, CvPlot* pPlot, YieldTypes iI, bool bConsiderFutureTech) const;
+
+	int ScoreYieldForReligionTimes100(YieldTypes eYield) const;
 
 	int ScoreCityForMissionary(CvCity* pCity, CvUnit* pUnit, ReligionTypes eSpreadReligion) const;
 	int ScoreCityForInquisitorOffensive(CvCity* pCity, CvUnit* pUnit, ReligionTypes eMyReligion) const;
