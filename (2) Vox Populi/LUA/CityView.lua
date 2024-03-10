69a7f0f109179d2e3533fc6b43283ba4314fd1f2
axatin <95587882+axatin@users.noreply.github.com>
2024-03-10 10:34:49 +0100

    Fix non-EUI yield tooltip bug

--- (2) Vox Populi/LUA/CityView.lua
+++ (2) Vox Populi/LUA/CityView.lua
@@ -1542,20 +1542,15 @@ function OnCityViewUpdate()
 
 		local freeSpecialists = pCity:GetRemainingFreeSpecialists();
 		if(freeSpecialists > 0) then
-			print("Free specialists!")
 			local freeSpecialtxt = tostring(freeSpecialists);
-			print(freeSpecialtxt);
 			Controls.FreeSpecialistLabel:SetText(freeSpecialtxt);
 			--Update suffix to use correct plurality.
 			Controls.FreeSpecialistLabelSuffix:LocalizeAndSetText( "TXT_KEY_CITYVIEW_FREESPECIALIST_TEXT", freeSpecialists );
 		else
-			print("No free specialists...")
 			local defSpecialist = tostring((GameDefines.UNHAPPINESS_PER_SPECIALIST / 100));
 			Controls.FreeSpecialistLabel:SetText(defSpecialist);
 			--Update suffix to use correct plurality.
 			Controls.FreeSpecialistLabelSuffix:LocalizeAndSetText("TXT_KEY_CITYVIEW_NOFREESPECIALIST_TEXT");
-			print(defSpecialist);
-			print("TXT_KEY_CITYVIEW_NOFREESPECIALIST_TEXT");
 		end
 
 		Controls.BuildingsHeader:SetToolTipString(strMaintenanceTT);
