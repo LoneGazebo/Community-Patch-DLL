048ade964037f11374f3c36f391032b46ba8fba8
jarcast <135236788+jarcast@users.noreply.github.com>
2024-07-11 18:44:38 +0100

    Aloe vera bonus resource placing compatibility
    Edit of Communitu_79a.lua and AssignStartingPlots.lua to support my Aloe Vera resource modmod. Changes are similar to Even More Resource modmod.
    I tested the changes beforehand and they work.

--- (2) Vox Populi/Mapscripts/Communitu_79a.lua
+++ (2) Vox Populi/Mapscripts/Communitu_79a.lua
@@ -921,6 +921,13 @@ function MapGlobals:New()
 			ASP:AddExtraBonusesToHillsRegions();
 
 			local resources_to_place = {};
+			
+			if ASP:IsAloeVeraResourceActive() then
+				resources_to_place = {
+					{ASP.aloevera_ID, 1, 100, 1, 2}
+				};
+				ASP:ProcessResourceList(10 * resMultiplier, ImpactLayers.LAYER_BONUS, tPlotList[PlotListTypes.FLAT_DESERT_NO_FEATURE], resources_to_place);
+			end
 
 			if ASP:IsEvenMoreResourcesActive() then
 				resources_to_place = {
