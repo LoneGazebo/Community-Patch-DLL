117821ead319ddbb9cac94a6ce5b99f535401938
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2024-12-06 10:12:15 -0500

    Fix minor UI annoyance

--- CvGameCoreDLL_Expansion2/CvPlayer.cpp
+++ CvGameCoreDLL_Expansion2/CvPlayer.cpp
@@ -16335,7 +16335,7 @@ int CvPlayer::GetNumUnitsSupplied(bool bCheckWarWeariness) const
 		iUnitSupply += GetNumUnitsSuppliedByPopulation();
 		iUnitSupply += GetUnitSupplyFromExpendedGreatPeople();
 
-		int iEmpireSizeReduction = max(GetNumEffectiveCities() * /*0 in CP, 5 in VP*/ GC.getMap().getWorldInfo().GetNumCitiesUnitSupplyMod(), 0);
+		int iEmpireSizeReduction = getNumCities() > 0 ? max(GetNumEffectiveCities(false) * /*0 in CP, 5 in VP*/ GC.getMap().getWorldInfo().GetNumCitiesUnitSupplyMod(), 0) : 0;
 		iUnitSupply *= 100;
 		iUnitSupply /= (100 + iEmpireSizeReduction);
 
