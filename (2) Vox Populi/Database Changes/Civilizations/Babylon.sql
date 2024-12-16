230543ba50981c766e44f1f3cc29b39024f6d640
azum4roll <azum4roll@users.noreply.github.com>
2024-12-16 15:15:07 +0800

    (8-06) Babylon
    added delta for Sabum Kibitum and Etemenanki

--- (2) Vox Populi/Database Changes/Civilizations/Babylon.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Babylon.sql
@@ -27,6 +27,32 @@ VALUES
 	('UNIT_BABYLONIAN_BOWMAN', 'PROMOTION_STRONGER_VS_DAMAGED');
 
 ----------------------------------------------------------
+-- Unique Unit: Sabum Kibitum (Spearman)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_BABYLON', 'UNITCLASS_SPEARMAN', 'UNIT_SABUM_KIBITUM');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_SPEARMAN'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_SPEARMAN') + 1
+WHERE Type = 'UNIT_SABUM_KIBITUM';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_SABUM_KIBITUM', 'PROMOTION_GAIN_EXPERIENCE'),
+	('UNIT_SABUM_KIBITUM', 'PROMOTION_LEGACY');
+
+----------------------------------------------------------
 -- Unique Building: Walls of Babylon (Walls)
 ----------------------------------------------------------
 UPDATE Buildings
@@ -42,3 +68,37 @@ INSERT INTO Building_YieldChanges
 	(BuildingType, YieldType, Yield)
 VALUES
 	('BUILDING_WALLS_OF_BABYLON', 'YIELD_SCIENCE', 3);
+
+----------------------------------------------------------
+-- Unique Building: Etemenanki (National Monument)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_BABYLON', 'BUILDINGCLASS_NATIONAL_EPIC', 'BUILDING_ETEMENANKI');
+
+UPDATE Building_YieldChanges
+SET Yield = (SELECT Yield FROM Building_YieldChanges WHERE BuildingType = 'BUILDING_NATIONAL_EPIC' AND YieldType = 'YIELD_CULTURE') + 3
+WHERE BuildingType = 'BUILDING_ETEMENANKI' AND YieldType = 'YIELD_CULTURE';
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_ETEMENANKI', 'YIELD_FOOD', 4),
+	('BUILDING_ETEMENANKI', 'YIELD_FAITH', 2);
+
+INSERT INTO Building_BuildingClassYieldChanges
+	(BuildingType, BuildingClassType, YieldType, YieldChange)
+VALUES
+	('BUILDING_ETEMENANKI', 'BUILDINGCLASS_MONUMENT', 'YIELD_SCIENCE', 1),
+	('BUILDING_ETEMENANKI', 'BUILDINGCLASS_SHRINE', 'YIELD_FOOD', 2);
+
+UPDATE Building_YieldFromBirth
+SET Yield = (SELECT Yield FROM Building_YieldFromBirth WHERE BuildingType = 'BUILDING_NATIONAL_EPIC' AND YieldType = 'YIELD_CULTURE') * 5 / 3
+WHERE BuildingType = 'BUILDING_ETEMENANKI' AND YieldType = 'YIELD_CULTURE';
+
+INSERT INTO Building_YieldFromPurchaseGlobal
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_ETEMENANKI', 'YIELD_FOOD', 10),
+	('BUILDING_ETEMENANKI', 'YIELD_SCIENCE', 10);
