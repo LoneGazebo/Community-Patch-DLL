66e72be763afc0f73ee1a4139f5ed762e5f273dc
azum4roll <azum4roll@users.noreply.github.com>
2024-12-23 17:34:56 +0800

    (8-12) Denmark added delta for Longship and Andelsbevægelse

--- (2) Vox Populi/Database Changes/Civilizations/Denmark.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Denmark.sql
@@ -43,6 +43,32 @@ VALUES
 	('UNIT_DANISH_BERSERKER', 'PROMOTION_CHARGE');
 
 ----------------------------------------------------------
+-- Unique Unit: Longship (Caravel)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_DENMARK', 'UNITCLASS_CARAVEL', 'UNIT_LONGSHIP');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_CARAVEL'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_CARAVEL') + 5
+WHERE Type = 'UNIT_LONGSHIP';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_LONGSHIP', 'PROMOTION_CAN_MOVE_AFTER_ATTACKING'),
+	('UNIT_LONGSHIP', 'PROMOTION_PIRACY');
+
+----------------------------------------------------------
 -- Unique Building: Runestone (Lighthouse)
 ----------------------------------------------------------
 INSERT INTO Civilization_BuildingClassOverrides
@@ -74,3 +100,40 @@ VALUES
 	('BUILDING_RUNESTONE', 'UNITCOMBAT_MELEE', 25),
 	('BUILDING_RUNESTONE', 'UNITCOMBAT_GUN', 25),
 	('BUILDING_RUNESTONE', 'UNITCOMBAT_NAVALMELEE', 25);
+
+----------------------------------------------------------
+-- Unique Building: Andelsbevægelse (Agribusiness)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_DENMARK', 'BUILDINGCLASS_AGRIBUSINESS', 'BUILDING_ANDELSBEVAEGELSE');
+
+UPDATE Buildings
+SET BuildingProductionModifier = 10
+WHERE Type = 'BUILDING_ANDELSBEVAEGELSE';
+
+DELETE FROM Building_ResourceQuantityRequirements WHERE BuildingType = 'BUILDING_ANDELSBEVAEGELSE';
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+--	('BUILDING_ANDELSBEVAEGELSE', 'YIELD_FOOD', 5),
+	('BUILDING_ANDELSBEVAEGELSE', 'YIELD_PRODUCTION', 3),
+	('BUILDING_ANDELSBEVAEGELSE', 'YIELD_GOLD', 3);
+
+INSERT INTO Building_ImprovementYieldChanges
+	(BuildingType, ImprovementType, YieldType, Yield)
+VALUES
+--	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_FARM', 'YIELD_PRODUCTION', 2),
+--	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_FARM', 'YIELD_GOLD', 2),
+	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_FARM', 'YIELD_FOOD', 2),
+--	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_PASTURE', 'YIELD_PRODUCTION', 2),
+--	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_PASTURE', 'YIELD_GOLD', 2),
+	('BUILDING_ANDELSBEVAEGELSE', 'IMPROVEMENT_PASTURE', 'YIELD_FOOD', 2);
+
+INSERT INTO Building_TerrainYieldChanges
+	(BuildingType, TerrainType, YieldType, Yield)
+VALUES
+	('BUILDING_ANDELSBEVAEGELSE', 'TERRAIN_GRASS', 'YIELD_FOOD', 1),
+	('BUILDING_ANDELSBEVAEGELSE', 'TERRAIN_PLAINS', 'YIELD_FOOD', 1);
