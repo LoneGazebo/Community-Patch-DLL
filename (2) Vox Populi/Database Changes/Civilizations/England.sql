dfd5bbb32366e4bfa9608e2da48ecad20790aaff
azum4roll <azum4roll@users.noreply.github.com>
2024-12-27 14:50:29 +0800

    (8-14) England
    changed leader expansion flavor
    modified trait
    added delta for Redcoat and Steam Mill
    added spy points and instant great admiral points for White Tower

--- (2) Vox Populi/Database Changes/Civilizations/England.sql
+++ (2) Vox Populi/Database Changes/Civilizations/England.sql
@@ -1,16 +1,24 @@
 ----------------------------------------------------------
--- Unique Ability: Perfidious Albion
+-- Unique Ability: Sun Never Sets
 ----------------------------------------------------------
 UPDATE Traits
 SET
 	ExtraSpies = 0,
-	ExtraEmbarkMoves = 1,
-	StartingSpies = 1,
-	NavalUnitMaintenanceModifier = -25,
-	SpySecurityModifier = 15,
-	SpyMoveRateModifier = 3,
-	SpyOffensiveStrengthModifier = 25
+	ExtraEmbarkMoves = 0,
+	NavalUnitMaintenanceModifier = -25
 WHERE Type = 'TRAIT_OCEAN_MOVEMENT';
+
+UPDATE Trait_FreePromotionUnitCombats SET PromotionType = 'PROMOTION_SUN_NEVER_SETS' WHERE TraitType = 'TRAIT_OCEAN_MOVEMENT';
+
+INSERT INTO Trait_FreePromotionUnitCombats
+	(TraitType, UnitCombatType, PromotionType)
+VALUES
+	('TRAIT_OCEAN_MOVEMENT', 'UNITCOMBAT_DIPLOMACY', 'PROMOTION_RIVAL_TERRITORY');
+
+INSERT INTO Trait_FreePromotionUnitClass
+	(TraitType, UnitClassType, PromotionType)
+VALUES
+	('TRAIT_OCEAN_MOVEMENT', 'UNITCLASS_ARCHAEOLOGIST', 'PROMOTION_RIVAL_TERRITORY');
 
 ----------------------------------------------------------
 -- Unique Unit: Ship of the Line (Frigate)
@@ -36,6 +44,31 @@ VALUES
 	('UNIT_ENGLISH_SHIPOFTHELINE', 'PROMOTION_SPLASH_2');
 
 ----------------------------------------------------------
+-- Unique Unit: Redcoat (Fusilier)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_ENGLAND', 'UNITCLASS_RIFLEMAN', 'UNIT_REDCOAT');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_RIFLEMAN'
+			)
+		)
+	)
+WHERE Type = 'UNIT_REDCOAT';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_REDCOAT', 'PROMOTION_SUN_NEVER_SETS'),
+	('UNIT_REDCOAT', 'PROMOTION_RULE_BRITANNIA');
+
+----------------------------------------------------------
 -- Unique Building: White Tower (Ironworks)
 ----------------------------------------------------------
 INSERT INTO Civilization_BuildingClassOverrides
@@ -46,6 +79,7 @@ VALUES
 UPDATE Buildings
 SET
 	Defense = 300,
+	ExtraSpies = 1,
 	GreatWorkSlotType = 'GREAT_WORK_SLOT_ART_ARTIFACT',
 	GreatWorkCount = 1,
 	FreeGreatWork = 'GREAT_WORK_THE_CROWN_JEWELS'
@@ -67,19 +101,22 @@ INSERT INTO Building_YieldFromSpyDefenseOrID
 	(BuildingType, YieldType, Yield)
 VALUES
 	('BUILDING_WHITE_TOWER', 'YIELD_GOLD', 40),
-	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 40);
+	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 40),
+	('BUILDING_WHITE_TOWER', 'YIELD_GREAT_ADMIRAL_POINTS', 40);
 
 INSERT INTO Building_YieldFromSpyAttack
 	(BuildingType, YieldType, Yield)
 VALUES
 	('BUILDING_WHITE_TOWER', 'YIELD_GOLD', 40),
-	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 40);
+	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 40),
+	('BUILDING_WHITE_TOWER', 'YIELD_GREAT_ADMIRAL_POINTS', 40);
 
 INSERT INTO Building_YieldFromSpyRigElection
 	(BuildingType, YieldType, Yield)
 VALUES
 	('BUILDING_WHITE_TOWER', 'YIELD_GOLD', 25),
-	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 25);
+	('BUILDING_WHITE_TOWER', 'YIELD_SCIENCE', 25),
+	('BUILDING_WHITE_TOWER', 'YIELD_GREAT_ADMIRAL_POINTS', 25);
 
 INSERT INTO Building_BuildingClassYieldChanges
 	(BuildingType, BuildingClassType, YieldType, YieldChange)
@@ -87,3 +124,37 @@ VALUES
 	('BUILDING_WHITE_TOWER', 'BUILDINGCLASS_ARMORY', 'YIELD_CULTURE', 1),
 	('BUILDING_WHITE_TOWER', 'BUILDINGCLASS_CASTLE', 'YIELD_CULTURE', 1),
 	('BUILDING_WHITE_TOWER', 'BUILDINGCLASS_CONSTABLE', 'YIELD_CULTURE', 1);
+
+----------------------------------------------------------
+-- Unique Building: Steam Mill (Factory)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_ENGLAND', 'BUILDINGCLASS_FACTORY', 'BUILDING_STEAM_MILL');
+
+UPDATE Buildings
+SET
+	PrereqTech = 'TECH_STEAM_POWER',
+	SpecialistCount = (SELECT SpecialistCount FROM Buildings WHERE Type = 'BUILDING_FACTORY') + 1,
+	NoUnhappfromXSpecialists = (SELECT NoUnhappfromXSpecialists FROM Buildings WHERE Type = 'BUILDING_FACTORY') + 1
+WHERE Type = 'BUILDING_STEAM_MILL';
+
+DELETE FROM Building_ResourceQuantityRequirements WHERE BuildingType = 'BUILDING_STEAM_MILL';
+
+UPDATE Building_YieldChangesPerPop
+SET Yield = (SELECT Yield FROM Building_YieldChangesPerPop WHERE BuildingType = 'BUILDING_FACTORY') * 2
+WHERE BuildingType = 'BUILDING_STEAM_MILL';
+
+UPDATE Building_ImprovementYieldChanges
+SET Yield = (SELECT Yield FROM Building_YieldChangesPerPop WHERE BuildingType = 'BUILDING_FACTORY') + 2
+WHERE BuildingType = 'BUILDING_STEAM_MILL';
+
+UPDATE Building_ResourceYieldChanges
+SET Yield = (SELECT Yield FROM Building_ResourceYieldChanges a WHERE a.BuildingType = 'BUILDING_FACTORY' AND a.YieldType = Building_ResourceYieldChanges.YieldType) + 1
+WHERE BuildingType = 'BUILDING_STEAM_MILL';
+
+INSERT INTO Building_GreatPersonPointFromConstruction
+	(BuildingType, GreatPersonType, EraType, Value)
+VALUES
+	('BUILDING_STEAM_MILL', 'GREATPERSON_ENGINEER', 'ERA_INDUSTRIAL', 10);
