7f5dbddb4dafb505039e22edb29d8f19a41a38b7
azum4roll <azum4roll@users.noreply.github.com>
2024-12-27 16:04:45 +0800

    (8-15) Ethiopia
    turned Mehal Sefari into a City-State gift
    added delta for Chewa and Oromo Cavalry
    added stats and abilities for Monolithic Church

--- (2) Vox Populi/Database Changes/Civilizations/Ethiopia.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Ethiopia.sql
@@ -13,26 +13,108 @@ VALUES
 	('TRAIT_BONUS_AGAINST_TECH', 'YIELD_FAITH', 1);
 
 ----------------------------------------------------------
--- Unique Unit: Mehal Sefari (Fusilier)
+-- Unique Unit: Chewa (Longswordsman)
 ----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_ETHIOPIA', 'UNITCLASS_LONGSWORDSMAN', 'UNIT_CHEWA');
+
 UPDATE Units
 SET
 	ObsoleteTech = (
 		SELECT ObsoleteTech FROM Units WHERE Type = (
 			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
-				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_RIFLEMAN'
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_LONGSWORDSMAN'
 			)
 		)
 	),
-	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_RIFLEMAN') + 4
-WHERE Type = 'UNIT_ETHIOPIAN_MEHAL_SEFARI';
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_LONGSWORDSMAN') + 2
+WHERE Type = 'UNIT_CHEWA';
 
 INSERT INTO Unit_FreePromotions
 	(UnitType, PromotionType)
 VALUES
-	('UNIT_ETHIOPIAN_MEHAL_SEFARI', 'PROMOTION_COVER_1'),
-	('UNIT_ETHIOPIAN_MEHAL_SEFARI', 'PROMOTION_DEFEND_NEAR_CAPITAL'),
-	('UNIT_ETHIOPIAN_MEHAL_SEFARI', 'PROMOTION_HOMELAND_GUARDIAN');
+	('UNIT_CHEWA', 'PROMOTION_MAIM'),
+	('UNIT_CHEWA', 'PROMOTION_HOOKED_WEAPON');
+
+----------------------------------------------------------
+-- Unique Unit: Oromo Cavalry (Cuirassier)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_ETHIOPIA', 'UNITCLASS_CUIRASSIER', 'UNIT_OROMO_CAVALRY');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_CUIRASSIER'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_CUIRASSIER') + 1,
+	RangedCombat = (SELECT RangedCombat FROM Units WHERE Type = 'UNIT_CUIRASSIER') + 2
+WHERE Type = 'UNIT_OROMO_CAVALRY';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_OROMO_CAVALRY', 'PROMOTION_FIELD_1'),
+	('UNIT_OROMO_CAVALRY', 'PROMOTION_ZEMENE_MESAFINT');
+
+----------------------------------------------------------
+-- Unique Improvement: Monolithic Church
+----------------------------------------------------------
+UPDATE Builds
+SET
+	PrereqTech = 'TECH_THEOLOGY',
+	Time = 900
+WHERE Type = 'BUILD_MONOLITHIC_CHURCH';
+
+INSERT INTO Unit_Builds
+	(UnitType, BuildType)
+VALUES
+	('UNIT_WORKER', 'BUILD_MONOLITHIC_CHURCH');
+
+INSERT INTO BuildFeatures
+	(BuildType, FeatureType, Time, Remove)
+VALUES
+	('BUILD_MONOLITHIC_CHURCH', 'FEATURE_FOREST', 400, 1),
+	('BUILD_MONOLITHIC_CHURCH', 'FEATURE_JUNGLE', 500, 1),
+	('BUILD_MONOLITHIC_CHURCH', 'FEATURE_MARSH', 500, 1);
+
+UPDATE Improvements
+SET
+	HillsMakesValid = 1,
+	NoTwoAdjacent = 1
+WHERE Type = 'IMPROVEMENT_MONOLITHIC_CHURCH';
+
+INSERT INTO Improvement_ResourceTypes
+	(ImprovementType, ResourceType)
+VALUES
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'RESOURCE_STONE');
+
+INSERT INTO Improvement_Yields
+	(ImprovementType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'YIELD_PRODUCTION', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'YIELD_FAITH', 1);
+
+INSERT INTO Improvement_AccomplishmentYieldChanges
+	(ImprovementType, AccomplishmentType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_POLICY_BRANCH_ANCIENT', 'YIELD_CULTURE', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_POLICY_BRANCH_MEDIEVAL', 'YIELD_FAITH', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_POLICY_BRANCH_INDUSTRIAL', 'YIELD_SCIENCE', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_CHOOSE_IDEOLOGY', 'YIELD_GOLD', 2),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_BELIEF_PANTHEON', 'YIELD_FAITH', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_BELIEF_FOUNDER', 'YIELD_CULTURE', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_BELIEF_FOLLOWER', 'YIELD_PRODUCTION', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_BELIEF_ENHANCER', 'YIELD_CULTURE', 1),
+	('IMPROVEMENT_MONOLITHIC_CHURCH', 'ACCOMPLISHMENT_BELIEF_REFORMATION', 'YIELD_GOLD', 1);
 
 ----------------------------------------------------------
 -- Unique Building: Stele (Monument)
