938a87c9ad7772e4da597dd68c9d677c890f5368
azum4roll <azum4roll@users.noreply.github.com>
2024-12-27 17:54:10 +0800

    (8-16) France added delta for SPAD S.VII and Salon

--- (2) Vox Populi/Database Changes/Civilizations/France.sql
+++ (2) Vox Populi/Database Changes/Civilizations/France.sql
@@ -42,6 +42,33 @@ VALUES
 	('UNIT_FRENCH_MUSKETEER', 'PROMOTION_LIGHTNING_WARFARE');
 
 ----------------------------------------------------------
+-- Unique Unit: SPAD S.VII (Triplane)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_FRANCE', 'UNITCLASS_TRIPLANE', 'UNIT_SPAD');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_TRIPLANE'
+			)
+		)
+	),
+	AirInterceptRange = (SELECT AirInterceptRange FROM Units WHERE Type = 'UNIT_TRIPLANE') + 1
+WHERE Type = 'UNIT_SPAD';
+
+DELETE FROM Unit_ResourceQuantityRequirements WHERE UnitType = 'UNIT_SPAD';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_SPAD', 'PROMOTION_AIR_LOGISTICS');
+
+----------------------------------------------------------
 -- Unique Improvement: Chateau
 ----------------------------------------------------------
 UPDATE Builds
@@ -72,3 +99,42 @@ VALUES
 	('IMPROVEMENT_CHATEAU', 'TECH_PRINTING_PRESS', 'YIELD_CULTURE', 1),
 	('IMPROVEMENT_CHATEAU', 'TECH_FLIGHT', 'YIELD_GOLD', 2),
 	('IMPROVEMENT_CHATEAU', 'TECH_FLIGHT', 'YIELD_CULTURE', 2);
+
+----------------------------------------------------------
+-- Unique Building: Salon (Gallery)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_FRANCE', 'BUILDINGCLASS_GALLERY', 'BUILDING_SALON');
+
+UPDATE Buildings
+SET
+	ThemingBonusHelp = 'TXT_KEY_SALON_THEMING_BONUS_HELP',
+	GPRateModifierPerLocalTheme = 10,
+	GreatWorkSlotType = 'GREAT_WORK_SLOT_ART_ARTIFACT',
+	GreatWorkCount = 2
+WHERE Type = 'BUILDING_SALON';
+
+UPDATE Building_YieldChanges
+SET Yield = (SELECT Yield FROM Building_YieldChanges WHERE BuildingType = 'BUILDING_GALLERY' AND YieldType = 'YIELD_CULTURE') + 3
+WHERE BuildingType = 'BUILDING_SALON' AND YieldType = 'YIELD_CULTURE';
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_SALON', 'YIELD_SCIENCE', 3),
+	('BUILDING_SALON', 'YIELD_TOURISM', 3);
+
+INSERT INTO Building_YieldChangesPerLocalTheme
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_SALON', 'YIELD_SCIENCE', 3);
+
+INSERT INTO Building_ThemingBonuses
+	(BuildingType, Description, MustBeArt, RequiresOwner, RequiresAnyButOwner, RequiresSamePlayer, SameEra)
+VALUES
+	('BUILDING_SALON', 'TXT_KEY_THEMING_BONUS_SALON_1', 1, 0, 1, 1, 1),
+	('BUILDING_SALON', 'TXT_KEY_THEMING_BONUS_SALON_2', 1, 0, 1, 1, 0),
+	('BUILDING_SALON', 'TXT_KEY_THEMING_BONUS_SALON_3', 1, 1, 0, 0, 1),
+	('BUILDING_SALON', 'TXT_KEY_THEMING_BONUS_SALON_4', 1, 1, 0, 0, 0);
