17f047ddfa76f7432c4cc8d54b38742fa588e123
axatin <95587882+axatin@users.noreply.github.com>
2025-01-06 00:18:13 +0100

    Fix CTD (#11475)

--- CvGameCoreDLL_Expansion2/CvReligionClasses.cpp
+++ CvGameCoreDLL_Expansion2/CvReligionClasses.cpp
@@ -5658,7 +5658,7 @@ void CvCityReligions::CityConvertsReligion(ReligionTypes eMajority, ReligionType
 		}
 
 		// Diplo implications (there must have been religion switch and a responsible party)
-		if (eMajority != eOldMajority && eResponsibleParty != NO_PLAYER && GET_PLAYER(eResponsibleParty).getTeam() != m_pCity->getTeam())
+		if (eMajority != eOldMajority && eResponsibleParty != NO_PLAYER && GET_PLAYER(eResponsibleParty).getTeam() != m_pCity->getTeam() && kOwnerPlayer.isMajorCiv())
 		{
 			// Is the city owner not the founder of this religion?
 			if (pNewReligion->m_eFounder != m_pCity->getOwner())
