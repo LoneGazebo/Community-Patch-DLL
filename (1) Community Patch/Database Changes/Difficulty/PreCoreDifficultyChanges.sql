0772c891ff9715b5b80383ef5e32c20949b6905b
axatin <95587882+axatin@users.noreply.github.com>
2025-01-06 09:53:41 +0100

    Remove gaps in HandicapInfos ID

--- (1) Community Patch/Database Changes/Difficulty/PreCoreDifficultyChanges.sql
+++ (1) Community Patch/Database Changes/Difficulty/PreCoreDifficultyChanges.sql
@@ -1,3 +1,8 @@
 -- Delete the data in the difficulty tables, replaced in CoreDifficultyChanges.xml and CoreDifficultyChanges.sql
 DELETE FROM HandicapInfo_Goodies;
 DELETE FROM HandicapInfos;
+
+-- make sure the rows inserted later have the correct IDs (don't remove this, it's needed for modpack creation to work)
+UPDATE sqlite_sequence
+SET seq = (SELECT COUNT(ID) FROM HandicapInfos) - 1
+WHERE name = 'HandicapInfos';
--- (2) Vox Populi/Database Changes/Difficulty/PreDifficultyChanges.sql
+++ (2) Vox Populi/Database Changes/Difficulty/PreDifficultyChanges.sql
@@ -5,3 +5,8 @@ DELETE FROM HandicapInfo_FreeTechs;
 DELETE FROM HandicapInfo_AIFreeTechs;
 DELETE FROM HandicapInfo_Goodies;
 DELETE FROM HandicapInfos;
+
+-- make sure the rows inserted later have the correct IDs (don't remove this, it's needed for modpack creation to work)
+UPDATE sqlite_sequence
+SET seq = (SELECT COUNT(ID) FROM HandicapInfos) - 1
+WHERE name = 'HandicapInfos';
