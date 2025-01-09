faa6c3d806662854a8861805097f33e651192071
azum4roll <azum4roll@users.noreply.github.com>
2025-01-09 12:23:15 +0800

    (8-31) Persia added delta for Qizilbash made Qizilbash always faith purchasable added stats and abilities for Charbagh

--- (2) Vox Populi/Database Changes/Civilizations/Persia.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Persia.sql
@@ -29,6 +29,80 @@ VALUES
 	('UNIT_PERSIAN_IMMORTAL', 'PROMOTION_ARMOR_PLATING_1');
 
 ----------------------------------------------------------
+-- Unique Unit: Qizilbash (Lancer)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_PERSIA', 'UNITCLASS_LANCER', 'UNIT_QIZILBASH');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_LANCER'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_LANCER') + 4
+WHERE Type = 'UNIT_QIZILBASH';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_QIZILBASH', 'PROMOTION_SAFAVI_AGITATOR');
+
+----------------------------------------------------------
+-- Unique Improvement: Charbagh
+----------------------------------------------------------
+UPDATE Builds
+SET
+	PrereqTech = 'TECH_THEOLOGY',
+	Time = 600
+WHERE Type = 'BUILD_CHARBAGH';
+
+INSERT INTO Unit_Builds
+	(UnitType, BuildType)
+VALUES
+	('UNIT_WORKER', 'BUILD_CHARBAGH');
+
+INSERT INTO BuildFeatures
+	(BuildType, FeatureType, Time, Remove)
+VALUES
+	('BUILD_CHARBAGH', 'FEATURE_FOREST', 400, 1),
+	('BUILD_CHARBAGH', 'FEATURE_JUNGLE', 500, 1),
+	('BUILD_CHARBAGH', 'FEATURE_MARSH', 500, 1);
+
+UPDATE Improvements
+SET
+	FreshWaterMakesValid = 1,
+	NoTwoAdjacent = 1,
+	GreatPersonRateModifier = 5
+WHERE Type = 'IMPROVEMENT_CHARBAGH';
+
+INSERT INTO Improvement_Yields
+	(ImprovementType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_CHARBAGH', 'YIELD_FOOD', 2),
+	('IMPROVEMENT_CHARBAGH', 'YIELD_PRODUCTION', 1),
+	('IMPROVEMENT_CHARBAGH', 'YIELD_GOLD', 1),
+	('IMPROVEMENT_CHARBAGH', 'YIELD_CULTURE', 1);
+
+INSERT INTO Improvement_GoldenAgeYields
+	(ImprovementType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_CHARBAGH', 'YIELD_PRODUCTION', 1),
+	('IMPROVEMENT_CHARBAGH', 'YIELD_CULTURE', 1);
+
+INSERT INTO Improvement_TechYieldChanges
+	(ImprovementType, TechType, YieldType, Yield)
+VALUES
+	('IMPROVEMENT_CHARBAGH', 'TECH_BANKING', 'YIELD_PRODUCTION', 1),
+	('IMPROVEMENT_CHARBAGH', 'TECH_BANKING', 'YIELD_CULTURE', 1),
+	('IMPROVEMENT_CHARBAGH', 'TECH_ELECTRICITY', 'YIELD_GOLD', 2);
+
+----------------------------------------------------------
 -- Unique Building: Satrap's Court (Courthouse)
 ----------------------------------------------------------
 UPDATE Civilization_BuildingClassOverrides
