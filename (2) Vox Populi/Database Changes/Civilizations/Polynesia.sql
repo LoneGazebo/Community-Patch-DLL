3320101b490a8ae8914e9edb34eca85684b7be3f
azum4roll <azum4roll@users.noreply.github.com>
2025-01-09 15:46:24 +0800

    (8-33) Polynesia
    modified delta for Maori Warrior
    added delta for Vaka Nui and Marae

--- (2) Vox Populi/Database Changes/Civilizations/Polynesia.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Polynesia.sql
@@ -27,27 +27,52 @@ FROM Units
 WHERE CombatClass = 'UNITCOMBAT_MELEE' OR CombatClass = 'UNITCOMBAT_GUN';
 
 ----------------------------------------------------------
--- Unique Unit: Maori Warrior (Longswordsman)
+-- Unique Unit: Maori Warrior (Pikeman)
 ----------------------------------------------------------
-UPDATE Civilization_UnitClassOverrides SET UnitClassType = 'UNITCLASS_SCOUT' WHERE UnitType = 'UNIT_POLYNESIAN_MAORI_WARRIOR';
+UPDATE Civilization_UnitClassOverrides SET UnitClassType = 'UNITCLASS_PIKEMAN' WHERE UnitType = 'UNIT_POLYNESIAN_MAORI_WARRIOR';
 
 UPDATE Units
 SET
 	ObsoleteTech = (
 		SELECT ObsoleteTech FROM Units WHERE Type = (
 			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
-				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_SCOUT'
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_PIKEMAN'
 			)
 		)
 	),
-	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_SCOUT') + 3
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_PIKEMAN') + 5
 WHERE Type = 'UNIT_POLYNESIAN_MAORI_WARRIOR';
 
 INSERT INTO Unit_FreePromotions
 	(UnitType, PromotionType)
 VALUES
-	('UNIT_POLYNESIAN_MAORI_WARRIOR', 'PROMOTION_AMPHIBIOUS'),
 	('UNIT_POLYNESIAN_MAORI_WARRIOR', 'PROMOTION_HAKA_WAR_DANCE');
+
+----------------------------------------------------------
+-- Unique Unit: Vaka Nui (Galley)
+----------------------------------------------------------
+INSERT INTO Civilization_UnitClassOverrides
+	(CivilizationType, UnitClassType, UnitType)
+VALUES
+	('CIVILIZATION_POLYNESIA', 'UNITCLASS_GALLEY', 'UNIT_VAKA_NUI');
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_VP_GALLEY'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_VP_GALLEY') + 2,
+	Moves = (SELECT Moves FROM Units WHERE Type = 'UNIT_VP_GALLEY') + 1
+WHERE Type = 'UNIT_VAKA_NUI';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_VAKA_NUI', 'PROMOTION_SUPPLY');
 
 ----------------------------------------------------------
 -- Unique Improvement: Moai
@@ -91,3 +116,24 @@ VALUES
 	('IMPROVEMENT_MOAI', 'TECH_ARCHAEOLOGY', 'YIELD_TOURISM', 1),
 	('IMPROVEMENT_MOAI', 'TECH_REFRIGERATION', 'YIELD_PRODUCTION', 1),
 	('IMPROVEMENT_MOAI', 'TECH_REFRIGERATION', 'YIELD_TOURISM', 1);
+
+----------------------------------------------------------
+-- Unique Building: Marae (Council)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_POLYNESIA', 'BUILDINGCLASS_COUNCIL', 'BUILDING_MARAE');
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+--	('BUILDING_MARAE', 'YIELD_SCIENCE', 1),
+	('BUILDING_MARAE', 'YIELD_CULTURE', 1);
+
+INSERT INTO Building_YieldFromConstruction
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_MARAE', 'YIELD_FOOD', 3),
+	('BUILDING_MARAE', 'YIELD_GOLD', 3),
+	('BUILDING_MARAE', 'YIELD_CULTURE', 3);
