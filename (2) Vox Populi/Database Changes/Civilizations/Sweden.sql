702af6289b59d418767d014c99ca700274a34118
azum4roll <azum4roll@users.noreply.github.com>
2025-01-17 17:47:16 +0800

    (8-41) Sweden added tundra start bias added delta for Hakkapeliitta, Nobel Committee, and Bastu

--- (2) Vox Populi/Database Changes/Civilizations/Sweden.sql
+++ (2) Vox Populi/Database Changes/Civilizations/Sweden.sql
@@ -5,7 +5,6 @@ UPDATE Traits
 SET
 	GreatPersonGiftInfluence = 0,
 	DOFGreatPersonModifier = 0,
-	XPBonusFromGGBirth = 15,
 	GreatGeneralExtraBonus = 15
 WHERE Type = 'TRAIT_DIPLOMACY_GREAT_PEOPLE';
 
@@ -20,6 +19,12 @@ INSERT INTO Trait_FreePromotionUnitCombats
 	(TraitType, UnitCombatType, PromotionType)
 VALUES
 	('TRAIT_DIPLOMACY_GREAT_PEOPLE', 'UNITCOMBAT_SIEGE', 'PROMOTION_MOBILITY');
+
+INSERT INTO Trait_BonusFromGPBirth
+	(TraitType, GreatPersonType, Heal, Experience)
+SELECT
+	'TRAIT_DIPLOMACY_GREAT_PEOPLE', Type, 10, 2
+FROM GreatPersons;
 
 ----------------------------------------------------------
 -- Unique Unit: Carolean (Fusilier)
@@ -42,3 +47,104 @@ INSERT INTO Unit_FreePromotions
 VALUES
 	('UNIT_SWEDISH_CAROLEAN', 'PROMOTION_MARCH'),
 	('UNIT_SWEDISH_CAROLEAN', 'PROMOTION_GRENADIER');
+
+----------------------------------------------------------
+-- Unique Unit: Hakkapeliitta (Cuirassier)
+----------------------------------------------------------
+UPDATE Civilization_UnitClassOverrides SET UnitClassType = 'UNITCLASS_CUIRASSIER' WHERE UnitType = 'UNIT_SWEDISH_HAKKAPELIITTA';
+
+UPDATE Units
+SET
+	ObsoleteTech = (
+		SELECT ObsoleteTech FROM Units WHERE Type = (
+			SELECT DefaultUnit FROM UnitClasses WHERE Type = (
+				SELECT UnitClassType FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_CUIRASSIER'
+			)
+		)
+	),
+	Combat = (SELECT Combat FROM Units WHERE Type = 'UNIT_CUIRASSIER') + 1,
+	RangedCombat = (SELECT RangedCombat FROM Units WHERE Type = 'UNIT_CUIRASSIER') + 2
+WHERE Type = 'UNIT_SWEDISH_HAKKAPELIITTA';
+
+INSERT INTO Unit_FreePromotions
+	(UnitType, PromotionType)
+VALUES
+	('UNIT_SWEDISH_HAKKAPELIITTA', 'PROMOTION_SENTRY'),
+	('UNIT_SWEDISH_HAKKAPELIITTA', 'PROMOTION_HAKKAA_PAALLE');
+
+----------------------------------------------------------
+-- Unique Building: Nobel Committee (Foreign Bureau)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_SWEDEN', 'BUILDINGCLASS_FOREIGN_BUREAU', 'BUILDING_NOBEL_COMMITTEE');
+
+UPDATE Buildings
+SET
+	PrereqTech = 'TECH_DYNAMITE',
+	GPExpendInfluence = 10
+WHERE Type = 'BUILDING_NOBEL_COMMITTEE';
+
+DELETE FROM Building_ClassesNeededInCity WHERE BuildingType = 'BUILDING_NOBEL_COMMITTEE';
+
+UPDATE Building_YieldChanges
+SET Yield = (SELECT Yield FROM Building_YieldChanges WHERE BuildingType = 'BUILDING_FOREIGN_BUREAU' AND YieldType = 'YIELD_CULTURE') + 4
+WHERE BuildingType = 'BUILDING_NOBEL_COMMITTEE' AND YieldType = 'YIELD_CULTURE';
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_NOBEL_COMMITTEE', 'YIELD_SCIENCE', 5);
+
+INSERT INTO Building_BuildingClassYieldChanges
+	(BuildingType, BuildingClassType, YieldType, YieldChange)
+VALUES
+	('BUILDING_NOBEL_COMMITTEE', 'BUILDINGCLASS_PUBLIC_SCHOOL', 'YIELD_SCIENCE', 2),
+	('BUILDING_NOBEL_COMMITTEE', 'BUILDINGCLASS_PUBLIC_SCHOOL', 'YIELD_CULTURE', 3);
+
+INSERT INTO Building_GreatWorkYieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_NOBEL_COMMITTEE', 'YIELD_SCIENCE', 2);
+
+INSERT INTO Building_ImprovementYieldChangesGlobal
+	(BuildingType, ImprovementType, YieldType, Yield)
+VALUES
+	('BUILDING_NOBEL_COMMITTEE', 'IMPROVEMENT_ACADEMY', 'YIELD_CULTURE', 3);
+
+INSERT INTO Building_YieldFromGPExpend
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_NOBEL_COMMITTEE', 'YIELD_GOLD', 50);
+
+----------------------------------------------------------
+-- Unique Building: Bastu (Baths)
+----------------------------------------------------------
+INSERT INTO Civilization_BuildingClassOverrides
+	(CivilizationType, BuildingClassType, BuildingType)
+VALUES
+	('CIVILIZATION_SWEDEN', 'BUILDINGCLASS_BATH', 'BUILDING_BASTU');
+
+UPDATE Buildings
+SET FreshWater = 0
+WHERE Type = 'BUILDING_BASTU';
+
+UPDATE Building_YieldChanges
+SET Yield = (SELECT Yield FROM Building_YieldChanges WHERE BuildingType = 'BUILDING_BATH' AND YieldType = 'YIELD_CULTURE') + 1
+WHERE BuildingType = 'BUILDING_BASTU' AND YieldType = 'YIELD_CULTURE';
+
+INSERT INTO Building_YieldChanges
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_BASTU', 'YIELD_FOOD', 3);
+
+INSERT INTO Building_YieldPerXTerrainTimes100
+	(BuildingType, TerrainType, YieldType, Yield)
+VALUES
+	('BUILDING_BASTU', 'TERRAIN_TUNDRA', 'YIELD_SCIENCE', 50);
+
+INSERT INTO Building_YieldFromGPExpend
+	(BuildingType, YieldType, Yield)
+VALUES
+	('BUILDING_BASTU', 'YIELD_CULTURE', 8);
