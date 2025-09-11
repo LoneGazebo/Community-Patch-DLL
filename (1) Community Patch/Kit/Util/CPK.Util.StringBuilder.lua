cfed4913e2b867aa6cfb48c5f9da6520f2c64169
Mykyta Smirnov <mykyta.smirnov.dev@gmail.com>
2025-09-11 19:01:41 +0200

    Fix lint of CPK.Util.StringBuilder

--- (1) Community Patch/Kit/Util/CPK.Util.StringBuilder.lua
+++ (1) Community Patch/Kit/Util/CPK.Util.StringBuilder.lua
@@ -1,82 +1,50 @@
-local _lua_table_insert = table.insert
-local _lua_table_concat = table.concat
-local _lua_table_remove = table.remove
-local _lua_setmetatable = setmetatable
-
-local StringBuilder = {}
-
---- @param self StringBuilder
---- @return string
-function StringBuilder.__tostring(self)
-	return self:Concat('\n')
-end
+local lua_table_insert = table.insert
+local lua_table_concat = table.concat
+local lua_table_remove = table.remove
+local lua_setmetatable = setmetatable
 
 --- @class StringBuilder
---- @field _chunks string[] # Internal storage of string chunks.
-StringBuilder.__index = {}
-
---- Creates a new TextBuilder instance.
---- @return StringBuilder
-function StringBuilder.New()
-	local this = {
-		_chunks = {},
-	} --[[@as StringBuilder]]
-
-	return _lua_setmetatable(this, StringBuilder)
-end
+--- @field protected items string[]
+local StringBuilderImpl = {}
 
 --- Returns string chunk at index if exists
 --- @param idx integer
 --- @return string | nil
-function StringBuilder.__index:At(idx)
-	return self._chunks[idx]
+function StringBuilderImpl:At(idx)
+	return self.items[idx]
 end
 
 --- Removes all string chunks.
 --- @return StringBuilder
-function StringBuilder.__index:Clear()
-	self._chunks = {}
+function StringBuilderImpl:Clear()
+	self.items = {}
 	return self
 end
 
---- Returns the number of string chunks.
+--- Returns the number of chunks.
 --- @return integer
-function StringBuilder.__index:Size()
-	return #(self._chunks)
-end
-
---- Returns the sum of lengths of all chunks.
---- @return integer
-function StringBuilder.__index:Length()
-	local len = 0
-	local chunks = self._chunks
-	local size = #(chunks)
-
-	for i = 1, size do
-		len = len + #(chunks[i])
-	end
-
-	return len
+function StringBuilderImpl:Size()
+	return #(self.items)
 end
 
 --- Checks if there are no string chunks.
 --- @return boolean
-function StringBuilder.__index:IsEmpty()
-	return self._chunks[1] == nil
+function StringBuilderImpl:Empty()
+	return self.items[1] == nil
 end
 
 --- Removes and returns the chunk at the given index.
 --- @param idx integer
 --- @return string | nil # String chunk that was removed or nil
-function StringBuilder.__index:Remove(idx)
-	return _lua_table_remove(self._chunks, idx)
+function StringBuilderImpl:Remove(idx)
+	return lua_table_remove(self.items, idx)
 end
 
 --- Appends a chunk to the end.
 --- @param str string
 --- @return StringBuilder
-function StringBuilder.__index:Append(str)
-	_lua_table_insert(self._chunks, str)
+function StringBuilderImpl:Append(str)
+	lua_table_insert(self.items, str)
 	return self
 end
 
@@ -84,15 +52,15 @@ end
 --- @param idx integer
 --- @param str string
 --- @return StringBuilder
-function StringBuilder.__index:Insert(idx, str)
-	_lua_table_insert(self._chunks, idx, str)
+function StringBuilderImpl:Insert(idx, str)
+	lua_table_insert(self.items, idx, str)
 	return self
 end
 
 --- Prepends a string to the beginning.
 --- @param str string
 --- @return StringBuilder
-function StringBuilder.__index:Prepend(str)
+function StringBuilderImpl:Prepend(str)
 	return self:Insert(1, str)
 end
 
@@ -102,8 +70,31 @@ end
 --- @param from? integer
 --- @param upto? integer
 --- @return string
-function StringBuilder.__index:Concat(separator, from, upto)
-	return _lua_table_concat(self._chunks, separator, from, upto)
+function StringBuilderImpl:Concat(separator, from, upto)
+	return lua_table_concat(self.items, separator, from, upto)
 end
 
-CPK.Util.StringBuilder = StringBuilder
+--- @class StringBuilderMeta
+local StringBuilderMeta = {}
+
+--- @package
+--- @param self StringBuilder
+--- @return string
+function StringBuilderMeta.__tostring(self)
+	return self:Concat('\n')
+end
+
+--- @package
+StringBuilderMeta.__index = StringBuilderImpl
+
+--- Creates a new StringBuilder instance.
+--- @return StringBuilder
+function StringBuilderMeta.New()
+	local this = {
+		items = {}
+	}
+
+	return lua_setmetatable(this, StringBuilderMeta)
+end
+
+CPK.Util.StringBuilder = StringBuilderMeta
