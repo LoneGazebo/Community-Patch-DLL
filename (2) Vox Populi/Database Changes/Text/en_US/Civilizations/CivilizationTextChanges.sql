3bd61f12ba5ad158645822754bfbec960d74c20e
Tom <thomas.sayer@btinternet.com>
2025-10-04 12:40:53 +0100

    Sweden
    For the new effect, changelog says "Units", old text was "Military Land Units"
    What's correct?

--- (2) Vox Populi/Database Changes/Text/en_US/Civilizations/CivilizationTextChanges.sql
+++ (2) Vox Populi/Database Changes/Text/en_US/Civilizations/CivilizationTextChanges.sql
@@ -950,7 +950,7 @@ SET Text = 'Lion of the North'
 WHERE Tag = 'TXT_KEY_TRAIT_DIPLOMACY_GREAT_PEOPLE_SHORT';
 
 UPDATE Language_en_US
-SET Text = 'Melee Land Units gain +20% [ICON_STRENGTH] Combat Strength [COLOR_POSITIVE_TEXT]when attacking[ENDCOLOR], and Siege Units gain +1 [ICON_MOVES] Movement. When a [ICON_GREAT_GENERAL] Great General is born, all Military Land Units fully heal and earn +15 XP. +15% [ICON_GREAT_GENERAL]/[ICON_GREAT_ADMIRAL] [COLOR_POSITIVE_TEXT]Leadership[ENDCOLOR] combat bonus.'
+SET Text = 'Melee Land Units gain +20% [ICON_STRENGTH] Combat Strength [COLOR_POSITIVE_TEXT]when attacking[ENDCOLOR], and Siege Units gain +1 [ICON_MOVES] Movement. Units heal 10 HP and earn +2 XP whenever a [ICON_GREAT_PEOPLE] Great Person is born. +15% [ICON_GREAT_GENERAL]/[ICON_GREAT_ADMIRAL] [COLOR_POSITIVE_TEXT]Leadership[ENDCOLOR] combat bonus.'
 WHERE Tag = 'TXT_KEY_TRAIT_DIPLOMACY_GREAT_PEOPLE';
 
 UPDATE Language_en_US
