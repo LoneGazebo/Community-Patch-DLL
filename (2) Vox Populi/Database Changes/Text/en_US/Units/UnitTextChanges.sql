02965eae5b22af563bb35f8a87c321676db7da90
Tom <thomas.sayer@btinternet.com>
2025-10-04 11:51:08 +0100

    Move Dromon text back to civ folder

--- (2) Vox Populi/Database Changes/Text/en_US/Units/UnitTextChanges.sql
+++ (2) Vox Populi/Database Changes/Text/en_US/Units/UnitTextChanges.sql
@@ -462,15 +462,6 @@ UPDATE Language_en_US
 SET Text = 'This Atomic Era melee unit can use its Survivalism I promotion to help it survive on the front line. When the opportunity presents itself, it can defeat a weakened enemy to earn points toward starting another [ICON_GOLDEN_AGE] Golden Age.'
 WHERE Tag = 'TXT_KEY_UNIT_BRAZILIAN_PRACINHA_STRATEGY';
 
--- Dromon
-UPDATE Language_en_US
-SET Text = ''
-WHERE Tag = 'TXT_KEY_UNIT_HELP_BYZANTINE_DROMON';
-
-UPDATE Language_en_US
-SET Text = 'The Dromon Greek Fire effect multiplies subsequent damage dealt to the Unit in the same turn, which better than giving +20% Combat Strength to the next attackers. Carefully consider this bonus to determine how many targets you can destroy in a turn and attack these targets with Dromons first before other units make subsequent attacks. In addition, The Dromon has significantly stronger base statistics, so generally it is a much better choice to build than melee ships. Consider conducting an early war with these ships due to their strength. Like Liburna, because it can move after attacking, you can attack Cities with many Dromons in the same turn.'
-WHERE Tag = 'TXT_KEY_UNIT_BYZANTINE_DROMON_STRATEGY';
-
 -- Norwegian Ski Infantry
 UPDATE Language_en_US
 SET Text = 'The Norwegian Ski Infantry can only be gifted by City-States.'
