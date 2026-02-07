8d19262da0b81bf1aac6596ef83cb4faad638ca2
axatin <95587882+axatin@users.noreply.github.com>
2026-02-07 14:26:46 +0100

    Fixed resource count bug (#12533)

--- CvGameCoreDLL_Expansion2/CvTeam.cpp
+++ CvGameCoreDLL_Expansion2/CvTeam.cpp
@@ -6436,6 +6436,10 @@ void CvTeam::setHasTech(TechTypes eIndex, bool bNewValue, PlayerTypes ePlayer, b
 						bool bUnlocksResource = false;
 						bool bResourceUnlocked = false;
 						TechTypes eTech = (TechTypes)pResourceInfo->getImproveTech();
+						if (eTech == NO_TECH)
+						{
+							eTech = (TechTypes)pResourceInfo->getTechReveal();
+						}
 						for (std::vector<PlayerTypes>::const_iterator iI = m_members.begin(); iI != m_members.end(); ++iI)
 						{
 							const PlayerTypes ePlayer = (PlayerTypes)*iI;
