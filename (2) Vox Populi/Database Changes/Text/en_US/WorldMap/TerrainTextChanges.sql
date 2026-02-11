744008ef2e04d148e4dd7f1f17b81775a86d3fc0
Tom <thomas.sayer@btinternet.com>
2026-02-11 21:56:41 +0000

    (9-24) Featureless Desert Yields
    Update (2) Vox Populi (v 17).modinfo

---
+++ (2) Vox Populi/Database Changes/Text/en_US/WorldMap/TerrainTextChanges.sql
@@ -0,0 +1,7 @@
+UPDATE Language_en_US SET
+Text = 'Deserts are land devoid of moisture; although many have sand, it is not a requirement. It is less useful than plains or grassland, but becomes more palatable once buildings are constructed that boost its yields, and those of the resources that can only be found on this type of terrain. Desert tiles next to rivers become Flood Plains, and the powerful Oasis feature only spawns here, and Farms may only be built on Desert with access to Fresh Water.'
+WHERE Tag = 'TXT_KEY_CIV5_TERRAIN_DESERT_TEXT';
+
+UPDATE Language_en_US SET
+Text = 'Tundra is the semi-frozen land found in the world''s colder climates. It is less useful than plains or grassland, but becomes more palatable once buildings are constructed that boost its yields, and those of the resources that can only be found on this type of terrain. It can sustain forests (but not jungle), and with enough investment they can become lucrative tiles. Farms may only be built on Tundra with access to Fresh Water.'
+WHERE Tag = 'TXT_KEY_CIV5_TERRAIN_TUNDRA_TEXT';
--- (2) Vox Populi/Database Changes/WorldMap/TerrainChanges.sql
+++ (2) Vox Populi/Database Changes/WorldMap/TerrainChanges.sql
@@ -2,6 +2,14 @@ UPDATE Terrains
 SET Movement = 2
 WHERE Type = 'TERRAIN_DESERT';
 
+INSERT INTO Terrain_Yields
+	(TerrainType, YieldType, Yield)
+VALUES
+	('TERRAIN_DESERT', 'YIELD_PRODUCTION', 1);
+
+-- otherwise Desert hills would go to +3
+UPDATE Terrain_HillsYieldChanges SET Yield = 1 WHERE TerrainType = 'TERRAIN_DESERT';
+
 UPDATE Terrains
 SET Movement = 2
 WHERE Type = 'TERRAIN_SNOW';
