9ac27ef40b32c230a175de34a7526050e32c9830
Tom <thomas.sayer@btinternet.com>
2026-02-14 15:54:31 +0000

    Add the global version
    Tested in game and (local) tooltip is working, as well as effect

--- (1) Community Patch/Core Files/Overrides/Includes/InfoTooltipInclude.lua
+++ (1) Community Patch/Core Files/Overrides/Includes/InfoTooltipInclude.lua
@@ -2284,6 +2284,7 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 		local tWLTKDModifiers = {};
 		local tInternalTRBoosts = {};
 		local tFaithPurchasableBuildings = {};
+		local tFaithPurchasableBuildingsGlobal = {};
 		for _, kYieldInfo in GameInfoCache("Yields") do
 			ExtractYieldTable(tSpecialistBoosts, "Building_SpecialistYieldChangesLocal", kYieldInfo, "SpecialistType");
 			ExtractYieldTable(tSpecialistBoostsGlobal, "Building_SpecialistYieldChanges", kYieldInfo, "SpecialistType");
@@ -2312,6 +2313,7 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 			ExtractSimpleYieldTable(tWLTKDModifiers, "Building_WLTKDYieldMod", kYieldInfo);
 			ExtractYieldTable(tResourceModifiers, "Building_ResourceYieldModifiers", kYieldInfo, "ResourceType");
 			ExtractSimpleYieldTable(tFaithPurchasableBuildings, "Building_ReligionYieldFromFaithPurchasableBuildings", kYieldInfo);
+			ExtractSimpleYieldTable(tFaithPurchasableBuildingsGlobal, "Building_ReligionYieldFromFaithPurchasableBuildingsGlobal", kYieldInfo);
 		end
 
 		for row in GameInfo.Building_YieldFromInternalTR{BuildingType = kBuildingInfo.Type} do
@@ -2355,6 +2357,7 @@ function GetHelpTextForBuilding(eBuilding, bExcludeName, _, bNoMaintenance, pCit
 		AddTooltipSimpleYieldModifierTable(tLocalAbilityLines, "TXT_KEY_PRODUCTION_BUILDING_YIELD_MODIFIER_GOLDEN_AGE", tGoldenAgeModifiers);
 		AddTooltipSimpleYieldModifierTable(tLocalAbilityLines, "TXT_KEY_PRODUCTION_BUILDING_YIELD_MODIFIER_WLTKD", tWLTKDModifiers);
 		AddTooltipSimpleYieldBoostTable(tLocalAbilityLines, "TXT_KEY_PRODUCTION_BUILDING_FAITH_PURCHASABLE_BUILDING_YIELDS", tFaithPurchasableBuildings);
+		AddTooltipSimpleYieldBoostTable(tGlobalAbilityLines, "TXT_KEY_PRODUCTION_BUILDING_FAITH_PURCHASABLE_BUILDING_YIELDS", tFaithPurchasableBuildingsGlobal);
 
 		-- Process boosts (both conversion rate and need modifier)
 		for row in GameInfo.Building_YieldFromProcessModifier{BuildingType = kBuildingInfo.Type} do
