038fd679ec32b2624adf996cfea289ff69faffd0
Daniel <danielk1709@gmail.com>
2026-03-02 12:03:47 +0100

    make bitops available if the lua binary is LuaJIT

--- CvGameCoreDLL_Expansion2/Lua/CvLuaSupport.cpp
+++ CvGameCoreDLL_Expansion2/Lua/CvLuaSupport.cpp
@@ -82,6 +82,7 @@ void LuaSupport::RegisterScriptData(lua_State* L)
 	"rawset = G.rawset;"
 	"rawget = G.rawget;"
 	"newproxy = G.newproxy;"
+	"if G.jit then JIT = true; bitops = G.require("bit") end;"
 
 	"FireaxisObjects={"
 		"UI = G.UI;"
