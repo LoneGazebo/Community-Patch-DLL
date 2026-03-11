b6794ea7d1812a574fe01239a11287c75243b1d4
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2026-03-11 14:51:48 -0400

    Remove MOD_BALANCE_VP for some C4DF features

--- CvGameCoreDLL_Expansion2/CvTeam.cpp
+++ CvGameCoreDLL_Expansion2/CvTeam.cpp
@@ -6936,7 +6936,7 @@ void CvTeam::setHasTech(TechTypes eIndex, bool bNewValue, PlayerTypes ePlayer, b
 
 			bFirstResource = false;
 
-			if (MOD_BALANCE_VP && GC.getGame().isOption(GAMEOPTION_NO_TECH_BROKERING))
+			if (GC.getGame().isOption(GAMEOPTION_NO_TECH_BROKERING))
 			{
 				SetTradeTech(eIndex, true);
 			}
@@ -7763,14 +7763,7 @@ void CvTeam::processTech(TechTypes eTech, int iChange, bool bNoBonus)
 	}
 	if(pTech->IsResearchAgreementTradingAllowed())
 	{
-		if (MOD_BALANCE_VP)
-		{
-			if (GC.getGame().isOption(GAMEOPTION_RESEARCH_AGREEMENTS))
-			{
-				ChangeResearchAgreementTradingAllowedCount(iChange);
-			}
-		}
-		else
+		if (!GC.getGame().isOption(GAMEOPTION_DISABLE_RESEARCH_AGREEMENTS))
 		{
 			ChangeResearchAgreementTradingAllowedCount(iChange);
 		}
@@ -8739,7 +8732,7 @@ void CvTeam::SetCurrentEra(EraTypes eNewValue)
 				}
 			}
 
-			if (pEraInfo->getVassalageEnabled() && !GC.getGame().isOption(GAMEOPTION_NO_VASSALAGE))
+			if (pEraInfo->getVassalageEnabled() && MOD_BALANCE_VP && GC.getGame().isOption(GAMEOPTION_ENABLE_VASSALAGE))
 			{
 				changeVassalageTradingAllowedCount(1);
 
@@ -9742,7 +9735,7 @@ bool CvTeam::canBecomeVassal(TeamTypes eTeam, bool bIgnoreAlreadyVassal) const
 	}
 
 	// Vassalage is disabled...
-	if(GC.getGame().isOption(GAMEOPTION_NO_VASSALAGE))
+	if(!MOD_BALANCE_VP || !GC.getGame().isOption(GAMEOPTION_ENABLE_VASSALAGE))
 	{
 		return false;
 	}
@@ -9827,7 +9820,7 @@ bool CvTeam::CanLiberateVassal(TeamTypes eTeam) const
 		return false;
 
 	// Vassalage is disabled...
-	if(GC.getGame().isOption(GAMEOPTION_NO_VASSALAGE))
+	if(!MOD_BALANCE_VP || !GC.getGame().isOption(GAMEOPTION_ENABLE_VASSALAGE))
 		return false;
 
 	// Must be a vassal of ours
@@ -10198,7 +10191,7 @@ void CvTeam::ChangeNumTurnsSinceVassalEnded(TeamTypes eTeam, int iChange)
 // Can we tax ePlayer right now?
 bool CvTeam::CanSetVassalTax(PlayerTypes ePlayer) const
 {
-	if (GC.getGame().isOption(GAMEOPTION_NO_VASSALAGE))
+	if (!MOD_BALANCE_VP || !GC.getGame().isOption(GAMEOPTION_ENABLE_VASSALAGE))
 		return false;
 
 	if (!isAlive() || !GET_PLAYER(ePlayer).isAlive() || !GET_PLAYER(ePlayer).isMajorCiv())
