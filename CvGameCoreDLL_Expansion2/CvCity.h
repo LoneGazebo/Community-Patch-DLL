517da44ad425b3cc37dc86e2ff062237c037d0fb
Tom <thomas.sayer@btinternet.com>
2026-05-16 11:22:53 +0100

    Remove GetCultureFromSpecialist method declaration

--- CvGameCoreDLL_Expansion2/CvCity.h
+++ CvGameCoreDLL_Expansion2/CvCity.h
@@ -478,8 +478,6 @@ public:
 	void SetReligiousPressureModifier(ReligionTypes eReligion, int iNewValue);
 	void ChangeReligiousPressureModifier(ReligionTypes eReligion, int iNewValue);
 
-	int GetCultureFromSpecialist(SpecialistTypes eSpecialist) const;
-
 	void UpdateOceanStatus();
 
 	const CvHandicapInfo& getHandicapInfo() const;
