LoneGazebo/Community-Patch-DLL

commit 2489e7368af73564bc7e4e5b9f007068494eab71

fix more switch warnings

CvGameCoreDLL_Expansion2/CvWorldBuilderMapLoader.cpp
@@ -1466,6 +1466,8 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 					lua_pushstring(L, "");
 				}
 			}
+			default:
+				UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.
 			}
 
 			lua_rawset(L, -3);
@@ -1528,6 +1530,8 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 					lua_pushstring(L, "");
 				}
 			}
+			default:
+				UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.
 			}
 		}
 
@@ -1597,6 +1601,8 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 								}
 							}
 						}
+						default:
+							UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.
 						}
 					}
 				}

CvGameCoreDLL_Expansion2/CvVotingClasses.cpp
@@ -4501,6 +4501,12 @@ void CvLeague::DoEnactProposalDiplomacy(ResolutionTypes eResolution, PlayerTypes
 				case CvLeagueAI::DESIRE_WEAK_LIKE:
 					GET_PLAYER(it->ePlayer).GetDiplomacyAI()->SetLikedTheirProposalValue(eProposer, /*-15*/ GD_INT_GET(OPINION_WEIGHT_WE_LIKED_THEIR_PROPOSAL_WEAK));
 					break;
+				case CvLeagueAI::DESIRE_NEUTRAL:
+				case CvLeagueAI::DESIRE_WEAK_DISLIKE:
+				case CvLeagueAI::DESIRE_DISLIKE:
+				case CvLeagueAI::DESIRE_STRONG_DISLIKE:
+				case CvLeagueAI::DESIRE_NEVER:
+					UNREACHABLE();
 				}
 			}
 			else if (eDesire < CvLeagueAI::DESIRE_NEUTRAL)
@@ -4522,6 +4528,12 @@ void CvLeague::DoEnactProposalDiplomacy(ResolutionTypes eResolution, PlayerTypes
 				case CvLeagueAI::DESIRE_WEAK_DISLIKE:
 					GET_PLAYER(it->ePlayer).GetDiplomacyAI()->SetLikedTheirProposalValue(eProposer, /*15*/ GD_INT_GET(OPINION_WEIGHT_WE_DISLIKED_THEIR_PROPOSAL));
 					break;
+				case CvLeagueAI::DESIRE_NEUTRAL:
+				case CvLeagueAI::DESIRE_WEAK_LIKE:
+				case CvLeagueAI::DESIRE_LIKE:
+				case CvLeagueAI::DESIRE_STRONG_LIKE:
+				case CvLeagueAI::DESIRE_ALWAYS:
+					UNREACHABLE();
 				}
 			}
 		}
@@ -4582,6 +4594,12 @@ void CvLeague::DoRepealProposalDiplomacy(int iTargetResolutionID, PlayerTypes eP
 				case CvLeagueAI::DESIRE_WEAK_LIKE:
 					GET_PLAYER(it->ePlayer).GetDiplomacyAI()->SetLikedTheirProposalValue(eProposer, /*-15*/ GD_INT_GET(OPINION_WEIGHT_WE_LIKED_THEIR_PROPOSAL_WEAK));
 					break;
+				case CvLeagueAI::DESIRE_NEUTRAL:
+				case CvLeagueAI::DESIRE_WEAK_DISLIKE:
+				case CvLeagueAI::DESIRE_DISLIKE:
+				case CvLeagueAI::DESIRE_STRONG_DISLIKE:
+				case CvLeagueAI::DESIRE_NEVER:
+					UNREACHABLE();
 				}
 			}
 			else if (eDesire < CvLeagueAI::DESIRE_NEUTRAL)
@@ -4603,6 +4621,12 @@ void CvLeague::DoRepealProposalDiplomacy(int iTargetResolutionID, PlayerTypes eP
 				case CvLeagueAI::DESIRE_WEAK_DISLIKE:
 					GET_PLAYER(it->ePlayer).GetDiplomacyAI()->SetLikedTheirProposalValue(eProposer, /*15*/ GD_INT_GET(OPINION_WEIGHT_WE_DISLIKED_THEIR_PROPOSAL));
 					break;
+				case CvLeagueAI::DESIRE_NEUTRAL:
+				case CvLeagueAI::DESIRE_WEAK_LIKE:
+				case CvLeagueAI::DESIRE_LIKE:
+				case CvLeagueAI::DESIRE_STRONG_LIKE:
+				case CvLeagueAI::DESIRE_ALWAYS:
+					UNREACHABLE();
 				}
 			}
 		}
@@ -7293,6 +7317,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 
 								// Exception: If desire multiplier is negative and this player is the proposer (AI has changed their mind), do nothing.
@@ -7373,6 +7399,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 
 								// Exception: If desire multiplier is negative and this player is the proposer (AI has changed their mind), do nothing.
@@ -7455,6 +7483,8 @@ void CvLeague::FinishSession()
 							case CvLeagueAI::DESIRE_ALWAYS:
 								iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 								break;
+							case CvLeagueAI::DESIRE_NEUTRAL:
+								break; // No multiplier.
 							}
 						}
 
@@ -7531,6 +7561,8 @@ void CvLeague::FinishSession()
 							case CvLeagueAI::DESIRE_ALWAYS:
 								iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 								break;
+							case CvLeagueAI::DESIRE_NEUTRAL:
+								break; // No multiplier.
 							}
 						}
 
@@ -7630,6 +7662,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 
 								// Exception: If desire multiplier is negative and this player is the proposer (AI has changed their mind), do nothing.
@@ -7715,6 +7749,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 
 								// Exception: If desire multiplier is negative and this player is the proposer (AI has changed their mind), do nothing.
@@ -7824,6 +7860,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 							}
 
@@ -7900,6 +7938,8 @@ void CvLeague::FinishSession()
 								case CvLeagueAI::DESIRE_ALWAYS:
 									iDesireMultiplier = /*400*/ GD_INT_GET(VOTING_HISTORY_SCORE_DESIRE_MULTIPLIER_OVERWHELMING);
 									break;
+								case CvLeagueAI::DESIRE_NEUTRAL:
+									break; // No multiplier.
 								}
 							}
 
@@ -10657,6 +10697,11 @@ CvLeagueAI::AlignmentLevels CvLeagueAI::EvaluateAlignment(PlayerTypes ePlayer, b
 	{
 		switch (pDiplo->GetDoFType(ePlayer))
 		{
+		case NO_DOF_TYPE:
+			UNREACHABLE();
+		case DOF_TYPE_UNTRUSTWORTHY:
+		case DOF_TYPE_NEW:
+			break;
 		case DOF_TYPE_FRIENDS:
 			iAlignment += 1;
 			break;
@@ -10684,6 +10729,8 @@ CvLeagueAI::AlignmentLevels CvLeagueAI::EvaluateAlignment(PlayerTypes ePlayer, b
 		{
 			switch (eTreatmentLevel)
 			{
+			case NO_VASSAL_TREATMENT:
+				UNREACHABLE();
 			case VASSAL_TREATMENT_CONTENT:
 				iAlignment += 4;
 				break;
@@ -10705,6 +10752,8 @@ CvLeagueAI::AlignmentLevels CvLeagueAI::EvaluateAlignment(PlayerTypes ePlayer, b
 		{
 			switch (eTreatmentLevel)
 			{
+			case NO_VASSAL_TREATMENT:
+				UNREACHABLE();
 			case VASSAL_TREATMENT_CONTENT:
 				iAlignment += 2;
 				break;
@@ -10754,6 +10803,7 @@ CvLeagueAI::AlignmentLevels CvLeagueAI::EvaluateAlignment(PlayerTypes ePlayer, b
 	case CIV_APPROACH_GUARDED:
 		iAlignment -= 1;
 		break;
+	case NO_CIV_APPROACH:
 	case CIV_APPROACH_AFRAID:
 	case CIV_APPROACH_NEUTRAL:
 		iAlignment += 0;
@@ -12336,6 +12386,8 @@ int CvLeagueAI::ScoreVoteChoiceYesNo(CvProposal* pProposal, int iChoice, bool bE
 						{
 							switch (eVassalTreatmentLevel)
 							{
+							case NO_VASSAL_TREATMENT:
+								UNREACHABLE();
 							case VASSAL_TREATMENT_CONTENT:
 								iExtra -= 500;
 								break;
@@ -12357,6 +12409,8 @@ int CvLeagueAI::ScoreVoteChoiceYesNo(CvProposal* pProposal, int iChoice, bool bE
 						{
 							switch (eVassalTreatmentLevel)
 							{
+							case NO_VASSAL_TREATMENT:
+								UNREACHABLE();
 							case VASSAL_TREATMENT_CONTENT:
 								iExtra -= 500;
 								break;
@@ -12866,6 +12920,10 @@ int CvLeagueAI::ScoreVoteChoiceYesNo(CvProposal* pProposal, int iChoice, bool bE
 			case PLAYER_PROXIMITY_NEIGHBORS:
 				iProximity = 250;
 				break;
+			case NO_PLAYER_PROXIMITY:
+			case PLAYER_PROXIMITY_DISTANT:
+			case PLAYER_PROXIMITY_FAR:
+				break; // No change.
 			}
 			// Do they have a resource we lack?
 			int iResourceDesire = 100 * GET_PLAYER(eTargetCityState).GetMinorCivAI()->GetNumResourcesMajorLacks(ePlayer);
@@ -13179,6 +13237,10 @@ int CvLeagueAI::ScoreVoteChoiceYesNo(CvProposal* pProposal, int iChoice, bool bE
 				case PLAYER_PROXIMITY_NEIGHBORS:
 					iAllyDesire += 400;
 					break;
+				case NO_PLAYER_PROXIMITY:
+				case PLAYER_PROXIMITY_DISTANT:
+				case PLAYER_PROXIMITY_FAR:
+					break; // No change.
 				}
 			}
 			// Do they have a resource we lack?

CvWorldBuilderMap/include/CvWorldBuilderMapModData.h
@@ -106,7 +106,7 @@ class ModType
 	friend ModDataEntry;
 
 public:
-	enum FieldType
+	enum CLOSED_ENUM FieldType
 	{
 		TYPE_INVALID = -1,
 		TYPE_BOOL = 0,

commit b154f749f92f4e8094fa8017fd0f4293cfe8f202

Fix 13 implicit-fallthrough bugs across 8 files
Add missing break statements to 13 switch cases that fell through
unintentionally, eliminating all 18 -Wimplicit-fallthrough warnings
(13 fixed + 5 intentional cascades left as-is).

Real bugs fixed (incorrect gameplay/crash behavior):
- CvDealClasses.cpp: DoF case fell through to Gold validation
- CvDiplomacyAI.cpp: FRIEND case fell through to ALLY, double-counting
  move-troops weights (+10/+5/-18 instead of +4/+2/-8)
- CvEspionageClasses.cpp: spy state with GetGoal==0 fell through to
  RIG_ELECTION percentage calculation
- CvNotifications.cpp: CITY_RANGE_ATTACK fell through to GOODY check;
  DEMAND_RESOURCE fell through to FOUND_PANTHEON logic
- CvPlayer.cpp: RESEARCH_AGREEMENT fell through to PILLAGE_GLOBAL,
  polluting research yields with pillage bonuses
- CvWorldBuilderMapLoader.cpp: TYPE_ENUM fell through to
  default:UNREACHABLE() causing crash on any enum mod data (3 sites,
  introduced by 2489e7368a adding default without break)

Harmless fallthroughs fixed (fell to break/return, no gameplay impact):
- CvCityStrategyAI.cpp: UNIT case fell through to break cases
- CvNotifications.cpp: POLICY fell through to default:break;
  hash-based case fell through to default:return false
- CvUnit.cpp: ESTABLISH_TRADE_ROUTE fell through to default:break

Intentional cascades NOT changed (5 sites):
- CvTacticalAI.cpp: A_PILLAGE/A_MOVE cascade into shared logic
- CvVotingClasses.cpp: vote resolution cascade (developer comment)

CvGameCoreDLL_Expansion2/CvCityStrategyAI.cpp
@@ -681,6 +681,7 @@ bool HaveSettlerInBuildables(const CvWeightedVector<CvCityBuildable>& choices)
 				UnitTypes eUnitType = (UnitTypes)choices.GetElement(i).m_iIndex;
 				if (GC.getUnitInfo(eUnitType)->IsFound())
 					return true;
+				break;
 			}
 			case NOT_A_CITY_BUILDABLE:
 			case CITY_BUILDABLE_BUILDING:

CvGameCoreDLL_Expansion2/CvDealClasses.cpp
@@ -484,6 +484,8 @@ bool CvDeal::IsPossibleToTradeItem(PlayerTypes ePlayer, PlayerTypes eToPlayer, T
 			// Already have a DoF?
 			if (pFromPlayer->GetDiplomacyAI()->IsDoFAccepted(eToPlayer))
 				return false;
+
+			break;
 		}
 
 	case TRADE_ITEM_GOLD:

CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
@@ -58105,6 +58105,7 @@ MoveTroopsResponseTypes CvDiplomacyAI::GetMoveTroopsRequestResponse(PlayerTypes 
 		viMoveTroopsWeights[MOVE_TROOPS_RESPONSE_ACCEPT] += 4;
 		viMoveTroopsWeights[MOVE_TROOPS_RESPONSE_NEUTRAL] += 2;
 		viMoveTroopsWeights[MOVE_TROOPS_RESPONSE_REFUSE] += -8;
+		break;
 	case CIV_OPINION_ALLY:
 		viMoveTroopsWeights[MOVE_TROOPS_RESPONSE_ACCEPT] += 6;	// Allies want to leave their allies alone more
 		viMoveTroopsWeights[MOVE_TROOPS_RESPONSE_NEUTRAL] += 3;

CvGameCoreDLL_Expansion2/CvEspionageClasses.cpp
@@ -4229,6 +4229,7 @@ int CvPlayerEspionage::GetPercentOfStateComplete(uint uiSpyIndex)
 		{
 			return -1;
 		}
+		break;
 	}
 	case SPY_STATE_RIG_ELECTION:
 		if(GC.getGame().GetTurnsBetweenMinorCivElections() != 0)

CvGameCoreDLL_Expansion2/CvNotifications.cpp
@@ -370,6 +370,7 @@ void CvNotifications::Dismiss(int iLookupIndex, bool bUserInvoked)
 				{
 					GC.GetEngineUserInterface()->SetPolicyNotificationSeen(true);
 				}
+				break;
 			}
 			default:
 				break;
@@ -1783,6 +1784,7 @@ bool CvNotifications::IsNotificationExpired(int iIndex)
 			}
 		}
 	}
+	break;
 
 	case NOTIFICATION_FOUND_PANTHEON:
 	{
@@ -1986,6 +1988,7 @@ bool CvNotifications::IsNotificationExpired(int iIndex)
 		if (!pCity->isPendingCapture())
 			return true;
 	}
+	break;
 
 	default:	// don't expire
 	{

CvGameCoreDLL_Expansion2/CvPlayer.cpp
@@ -26164,6 +26164,8 @@ void CvPlayer::doInstantYield(InstantYieldType iType, bool bCityFaith, GreatPers
 				{
 					if (eYield == ePassYield)
 						iValue += iPassYield;
+
+					break;
 				}
 				case INSTANT_YIELD_TYPE_PILLAGE_GLOBAL:
 				{

CvGameCoreDLL_Expansion2/CvUnit.cpp
@@ -30965,6 +30965,7 @@ bool CvUnit::CanDoInterfaceMode(InterfaceModeTypes eInterfaceMode, bool bTestVis
 		{
 			return true;
 		}
+		break;
 
 	default:
 		break;

CvGameCoreDLL_Expansion2/CvWorldBuilderMapLoader.cpp
@@ -1486,6 +1486,7 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 				{
 					lua_pushstring(L, "");
 				}
+				break;
 			}
 			default:
 				UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.
@@ -1550,6 +1551,7 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 				{
 					lua_pushstring(L, "");
 				}
+				break;
 			}
 			default:
 				UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.
@@ -1621,6 +1623,7 @@ int CvWorldBuilderMapLoader::LoadModData(lua_State* L)
 									lua_rawset(L, -3);
 								}
 							}
+							break;
 						}
 						default:
 							UNREACHABLE(); // This would typically indicate corruption so termination seems reasonable.