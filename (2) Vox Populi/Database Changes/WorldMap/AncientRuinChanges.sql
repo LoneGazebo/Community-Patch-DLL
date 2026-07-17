eedb7903d5366193dfb85a93fe53bb7b79ba4542
axatin <95587882+axatin@users.noreply.github.com>
2026-07-17 08:31:19 +0200

    Database cleanup
    - Move unused new goody huts from (1) to (2)
    - Apply table type fixes before all other table changes
    - Rename new missions files for consistency

--- (2) Vox Populi/Database Changes/WorldMap/AncientRuinChanges.sql
+++ (2) Vox Populi/Database Changes/WorldMap/AncientRuinChanges.sql
@@ -6,10 +6,8 @@ SET
 WHERE Type = 'GOODY_MAP';
 
 -- Ruin yields
-UPDATE GoodyHuts SET Production = 50 WHERE Type = 'GOODY_PRODUCTION';
 UPDATE GoodyHuts SET Gold = 100, NumGoldRandRolls = 0 WHERE Type = 'GOODY_GOLD';
 UPDATE GoodyHuts SET Culture = 15 WHERE Type = 'GOODY_CULTURE';
 UPDATE GoodyHuts SET PantheonFaith = 0, PantheonPercent = 60 WHERE Type = 'GOODY_PANTHEON_FAITH';
 UPDATE GoodyHuts SET ProphetPercent = 25 WHERE Type = 'GOODY_PROPHET_FAITH';
 UPDATE GoodyHuts SET Experience = 20 WHERE Type = 'GOODY_EXPERIENCE';
-UPDATE GoodyHuts SET Promotion = 'PROMOTION_LOST_PROTOTYPE' WHERE Type = 'GOODY_LOST_PROTOTYPE';
